d48b7d6f2e299f1e37bbbace23160eac718188b7
Steve Starke <steve.starke@cloudandheat.com>
2020-11-03 20:18:27 +0100

    fix python command
    * the warning is triggered if only python3 is available
    * it is currently not recommended to symlink python to
      python3 (refer https://legacy.python.org/dev/peps/pep-0394/)

--- actions/wg-up.sh
+++ actions/wg-up.sh
@@ -19,7 +19,7 @@ if [ -n "$wg_existing_route" ] && [ -z "${MANAGED_K8S_IGNORE_WIREGUARD_ROUTE:-}"
 fi
 
 ipam_path="$cluster_repository/config/wireguard_ipam.toml"
-if python -c "import toml, sys; sys.exit(any(user['ident'] == '$wg_user' for user in toml.load('$ipam_path')['users']))" ; then
+if python3 -c "import toml, sys; sys.exit(any(user['ident'] == '$wg_user' for user in toml.load('$ipam_path')['users']))" ; then
     warningf 'failed to find wireguard user %s in trampoline configuration' "$wg_user" >&2
 fi
 
