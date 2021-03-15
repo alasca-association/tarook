85a9c17f57821cda0b144f19d78036f85a575322
Steve Starke <steve.starke@cloudandheat.com>
2021-03-15 14:46:41 +0100

    Add DualStack support to wireguard
    The wg-iup.sh script has been updated to support IPv6 addresses.
    The wireguard.conf has been modified to enable IPv6 forwarding if
    DualStack is enabled. The client and server configuration templates
    are adjusted to add IPv6 addresses and prefix if DualStack support
    is enabled.
    
    For the connection to cluster nodes, it is necessary that radvd is
    configured on the gateways.
    
    Issue: #227

--- actions/wg-up.sh
+++ actions/wg-up.sh
@@ -8,19 +8,27 @@ if [ "${WG_USAGE:-true}" == "true" ]; then
     validate_wireguard
 
     wg_subnet="$(jq -r .subnet_cidr "$terraform_state_dir/config.tfvars.json")"
+    wg_subnet_v6="$(jq -r .subnet_v6_cidr "$terraform_state_dir/config.tfvars.json")"
     # the grep is there to ignore any routes going via the interface we're going to
     # take down later either way
     wg_existing_route="$(ip route show to "$wg_subnet" | grep -v "dev $wg_interface" || true)"
-    if [ -n "$wg_existing_route" ] && [ -z "${MANAGED_K8S_IGNORE_WIREGUARD_ROUTE:-}" ]; then
-        errorf 'route to wireguard network %s exists already: %s' "$wg_subnet" "$wg_existing_route" >&2
-        hintf 'disable the responsible interface' >&2
+    wg_existing_v6_route="$(ip -6 route show to "$wg_subnet_v6" | grep -v "dev $wg_interface" || true)"
+    if { [ -n "$wg_existing_route" ] || [ -n "$wg_existing_v6_route" ]; } \
+            && [ -z "${MANAGED_K8S_IGNORE_WIREGUARD_ROUTE:-}" ]; then
+        if [ -n "$wg_existing_route" ]; then
+            errorf 'route to wireguard network %s exists already: %s' "$wg_subnet" "$wg_existing_route" >&2
+        fi
+        if [ -n "$wg_existing_v6_route" ]; then
+            errorf 'route to wireguard network %s exists already: %s' "$wg_subnet_v6" "$wg_existing_v6_route" >&2
+        fi
+        hintf 'disable the responsible interface(s)' >&2
         # shellcheck disable=SC2016
         hintf '(or set $MANAGED_K8S_IGNORE_WIREGUARD_ROUTE if you know what you'"'"'re doing)' >&2
         exit 2
     fi
 
     ipam_path="$cluster_repository/config/wireguard_ipam.toml"
-    if python3 -c "import toml, sys; sys.exit(any(user['ident'] == '$wg_user' for user in toml.load('$ipam_path')['wg_users']))" ; then
+    if python3 -c "import toml, sys; sys.exit(any(user['ident'] == '$wg_user' for user in toml.load('$ipam_path')['users']))" ; then
         warningf 'failed to find wireguard user %s in trampoline configuration' "$wg_user" >&2
     fi
 
