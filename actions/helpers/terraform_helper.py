6061563c8b4d64933a1b8532ec4e4ad87596d093
Yannic Ahrens <yannic.ahrens@cloudandheat.com>
2021-09-16 10:42:17 +0200

    tf: prevent changing cluster_name on the fly
    TF will happily update all existing resource names if `cluster_name` is
    modified. This can lead to trouble. At least the hostname of nodes might
    update (or not). On one occasion I managed to confuse tf so much that it
    wanted to re-create all resources. This is simply unacceptable, even if
    the kraken has been released.
    
    Based upon this commit, terraform_helper will compare the config.tfvars.json
    with the new one and *not* update `config.tfvars.json` upon a mismatch.
    Instead you have to do an explicit destroy/apply cycle.

--- actions/helpers/terraform_helper.py
+++ actions/helpers/terraform_helper.py
@@ -2,12 +2,34 @@
 
 import pathlib
 import json
+import sys
 
 TFVARS_DIR = pathlib.Path("terraform")
 TFVARS_FILE = TFVARS_DIR / "config.tfvars.json"
 
 
+def get_current_config():
+    if not TFVARS_DIR.exists() or not TFVARS_FILE.exists():
+        return {}
+    with open(TFVARS_FILE, "r") as f:
+        return json.load(f)
+
+
+def check_for_changed_cluster_name(current_config, new_config):
+    cluster_name = current_config.get("cluster_name", "")
+    candidate_cluster_name = new_config.get("cluster_name", "")
+    if cluster_name != candidate_cluster_name:
+        print("[FATAL] Will not update terraform config because there is a mismatch between the deployed and future cluster_name. This would cause death and destruction.", file=sys.stderr) # NOQA
+        if cluster_name == "":
+            print("[FATAL] `cluster_name` wasn't set before, so remove the field from the config.toml", file=sys.stderr) # NOQA
+        else:
+            print(f"[FATAL] Set `cluster_name` to {cluster_name}. Your suggested change was {candidate_cluster_name} and is unacceptable.", file=sys.stderr) # NOQA
+        sys.exit(1)
+
+
 def deploy_terraform_config(terraform_config):
+    current_config = get_current_config()
+    check_for_changed_cluster_name(current_config, terraform_config)
     TFVARS_DIR.mkdir(exist_ok=True)
     with open(TFVARS_FILE, "w") as fout:
         json.dump(terraform_config, fout)
