a7492b6f7085d01c6d0a4db5b8f7904e0b559460
Yannic Ahrens <yannic.ahrens@cloudandheat.com>
2021-12-21 19:05:45 +0100

    Add action for system updates of initialized nodes
    Installing system updates is generally a disruptive task because it's
    hard to reason about what's going to happen. Is the docker runtime
    restarted? Systemd doing some fancy things to the network?
    `update_system` is therefore only allowed if the node isn't used yet
    (evaluated by a heuristic) or if `MANAGED_K8S_RELEASE_THE_KRAKEN=true`
    is set. The latter also allows the playbook to do other scary things so
    I hardly invoke stage-2 and stage-3 with no holds barred.
    
    "But you could limit execution to a subset of tasks!" Sure, I could. But
    it's easy to forget the tag and the current implementation actually harms
    performance. On the set of existing nodes, the package cache is updated
    *in serial* which takes a long time with a sufficient number of nodes.
    
    In order to resolve the above, this commit
    
    - does no longer update nodes anyhow that are already initialized (both
      gateways and k8s nodes)
    - adds another action `system_update_nodes.sh` if, in case you're
      using the LCM for the updates

---
+++ actions/system_update_nodes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+actions_dir="$(dirname "$0")"
+
+# Ensure that the latest config is deployed to the inventory
+python3 "$actions_dir/update_inventory.py"
+
+# shellcheck source=actions/lib.sh
+. "$actions_dir/lib.sh"
+
+cd "$ansible_playbook"
+ansible-galaxy install -r requirements.yaml
+ansible_playbook -i "$ansible_inventoryfile_02" -i "$ansible_inventoryfile_03" 06_system_update_nodes.yaml "$@"
