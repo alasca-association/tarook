5e5a7e0222170d8148d0bbcfddd2b4ff1e8a2cfd
Steve Starke <steve.starke@cloudandheat.com>
2022-02-23 17:49:52 +0100

    CI: Ensure system node updates are executed
    Although that does not ensure full functionality, at least we can ensure
    that the action script is basically (syntactically) working

--- actions/system_update_nodes.sh
+++ actions/system_update_nodes.sh
@@ -8,6 +8,12 @@ python3 "$actions_dir/update_inventory.py"
 # shellcheck source=actions/lib.sh
 . "$actions_dir/lib.sh"
 
-cd "$ansible_playbook"
-ansible-galaxy install -r requirements.yaml
-ansible_playbook -i "$ansible_inventoryfile_02" -i "$ansible_inventoryfile_03" 06_system_update_nodes.yaml "$@"
+# Installing updates can be highly disruptive
+require_disruption
+
+# Bring the wireguard interface up if configured so
+"$actions_dir/wg-up.sh"
+
+cd "$ansible_k8s_base_playbook"
+ansible-galaxy install -r "$ansible_directory/requirements.yaml"
+ansible_playbook -i "$ansible_inventoryfile_02" -i "$ansible_inventoryfile_03" system_update_nodes.yaml "$@"
