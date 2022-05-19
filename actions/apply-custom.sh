70c63cef340223ef2826f6a315ffaf8e1171b72f
Tony Fiedler <tony.fiedler@cloudandheat.com>
2022-05-19 11:12:33 +0200

    Allow to specify vars for custom stage in `config.toml`
    These variables can be used in plays in the custom stage.
    
    Copy the cluster-repo-specific inventory file from stage5.
    This e.g. allows us referencing to the orchestrator host group
    in custom plays.
    
    Each host of the custom stage has access to the custom
    vars specified in the dedicated section of the `config.toml`.
    
    Closes: #462

--- actions/apply-custom.sh
+++ actions/apply-custom.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 actions_dir="$(dirname "$0")"
 
+# Ensure that the latest config is deployed to the inventory
+python3 "$actions_dir/update_inventory.py"
+
 # shellcheck source=actions/lib.sh
 . "$actions_dir/lib.sh"
 
