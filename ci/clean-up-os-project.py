7faf5f9606c5d79cf1cf775c590f049b7b2e79a4
Steve Starke <steve.starke@cloudandheat.com>
2022-06-16 10:56:07 +0200

    OS-Cleanup: Process outcome of deletion API calls
    This commit extends the OpenStack project clean up script such that the
    return code of a resource deletion API call is printed and the script
    raises an Exception if the deletion of a resource failed. It is also
    ensured that the OpenStack connection is properly closed on failures.
    
    Unfortunately, not every call provides a return code.

--- ci/clean-up-os-project.py
+++ ci/clean-up-os-project.py
@@ -2,7 +2,7 @@
 
 import os
 import openstack
-
+import typing
 
 def create_os_connection():
     username = os.environ['OS_USERNAME']
@@ -24,6 +24,13 @@ def create_os_connection():
     )
 
 
+def process_deletion_outcome(outcome: typing.Optional[bool]):
+    # Print message about outcome and exit if failed
+    print(f"{'Succeeded' if outcome else 'Failed' if outcome is not None else 'Unknown'}")  # noqa: E501
+    if not outcome and outcome is not None:
+        raise RuntimeError('Deletion of the OpenStack resource failed.')
+
+
 def main():
     """
     This script cleans up the currently sourced OpenStack project from all
@@ -33,72 +40,89 @@ def main():
 
     print("===\nConnect to OpenStack\n===")
     conn = create_os_connection()
+    try:
+        print("---\nDelete Floating IPs\n---")
+        for floating_ip in conn.list_floating_ips():
+            print(f"Delete Floating IP {floating_ip['id']}")
+            process_deletion_outcome(
+                conn.delete_floating_ip(floating_ip)
+            )
 
-    print("---\nDelete Floating IPs\n---")
-    for floating_ip in conn.list_floating_ips():
-        print(f"Delete Floating IP {floating_ip['id']}")
-        conn.delete_floating_ip(floating_ip['id'])
+        print("---\nDelete Server\n---")
+        for server in conn.compute.servers():
+            print(f"Delete server {server['id']}")
+            process_deletion_outcome(
+                conn.compute.delete_server(server)
+            )
 
-    print("---\nDelete Server\n---")
-    for server in conn.compute.servers():
-        print(f"Delete server {server['id']}")
-        conn.compute.delete_server(server)
+        print("---\nDelete Routers\n---")
+        for router in conn.network.routers():
+            for router_interface in conn.list_router_interfaces(router):
+                print(
+                    f"Delete interface {router_interface['id']}",
+                    f"from {router['id']}")
+                conn.remove_router_interface(
+                    router, port_id=router_interface['id'])
+            print(f"Delete router {router['id']}")
+            process_deletion_outcome(
+                conn.delete_router(router)
+            )
 
-    print("---\nDelete Routers\n---")
-    for router in conn.network.routers():
-        for router_interface in conn.list_router_interfaces(router):
-            print(
-                f"Delete interface {router_interface['id']}",
-                f"from {router['id']}")
-            conn.remove_router_interface(
-                router, port_id=router_interface['id'])
-        print(f"Delete router {router['id']}")
-        conn.delete_router(router)
+        print("---\nDelete Ports\n---")
+        for port in conn.network.ports():
+            print(f"Delete port {port['id']}")
+            process_deletion_outcome(
+                conn.delete_port(port['id'])
+            )
 
-    print("---\nDelete Ports\n---")
-    for port in conn.network.ports():
-        print(f"Delete port {port['id']}")
-        if not conn.delete_port(port['id']):
-            print(f"WARNING: Deletion of port {port['id']} failed.")
+        print("---\nDelete Networks\n---")
+        for network in conn.list_networks():
+            # do not try to delete public ipv4 network
+            if network['id'] == "585ec5ec-5993-4042-93b9-264b0d82ac8e":
+                continue
+            # getattr(network, "subnets", default=getattr(network, "subnet_ids"))
+            # throws an exception, the getattr method is overwritten by the class
+            # and therefore providing default is not working as expected.
+            try:
+                subnets = getattr(network, "subnets")
+            except AttributeError:
+                subnets = getattr(network, "subnet_ids")
+            for subnet in subnets:
+                print(f"Delete subnet {subnet}")
+                process_deletion_outcome(
+                    conn.network.delete_subnet(subnet)
+                )
+            print(f"Delete network {network['id']}")
+            process_deletion_outcome(
+                conn.network.delete_network(network)
+            )
 
-    print("---\nDelete Networks\n---")
-    for network in conn.list_networks():
-        # do not try to delete public ipv4 network
-        if network['id'] == "585ec5ec-5993-4042-93b9-264b0d82ac8e":
-            continue
-        # getattr(network, "subnets", default=getattr(network, "subnet_ids"))
-        # throws an exception, the getattr method is overwritten by the class
-        # and therefore providing default is not working as expected.
-        try:
-            subnets = getattr(network, "subnets")
-        except AttributeError:
-            subnets = getattr(network, "subnet_ids")
-        for subnet in subnets:
-            print(f"Delete subnet {subnet}")
-            conn.network.delete_subnet(subnet)
-        print(f"Delete network {network['id']}")
-        conn.network.delete_network(network)
+        print("---\nDelete Volumes\n---")
+        for volume in conn.list_volumes():
+            print(f"Delete volume {volume['id']}")
+            process_deletion_outcome(
+                conn.block_storage.delete_volume(volume)
+            )
 
-    print("---\nDelete Volumes\n---")
-    for volume in conn.list_volumes():
-        print(f"Delete volume {volume['id']}")
-        conn.block_storage.delete_volume(volume)
+        print("---\nDelete Security Groups\n---")
+        for security_group in conn.list_security_groups():
+            # do not delete the default security group
+            if security_group['name'] == "default":
+                continue
+            print(f"Delete security group {security_group['id']}")
+            process_deletion_outcome(
+                conn.delete_security_group(security_group)
+            )
 
-    print("---\nDelete Security Groups\n---")
-    for security_group in conn.list_security_groups():
-        # do not delete the default security group
-        if security_group['name'] == "default":
-            continue
-        print(f"Delete security group {security_group['id']}")
-        conn.delete_security_group(security_group['id'])
-
-    print("---\nDelete Server Groups\n---")
-    for server_group in conn.list_server_groups():
-        print(f"Delete server group {server_group['id']}")
-        conn.delete_server_group(server_group['id'])
-
-    print("===\nDisconnect from OpenStack\n===")
-    conn.close()
+        print("---\nDelete Server Groups\n---")
+        for server_group in conn.list_server_groups():
+            print(f"Delete server group {server_group['id']}")
+            process_deletion_outcome(
+                conn.delete_server_group(server_group)
+            )
+    finally:
+        print("===\nDisconnect from OpenStack\n===")
+        conn.close()
 
 
 if __name__ == "__main__":
