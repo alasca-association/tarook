933333073ea2eef6ae4a85332be29a409ca0f9db
Jonas Schäfer <jonas.schaefer@cloudandheat.com>
2022-07-15 10:50:50 +0200

    load-signed-intermediates: add support for loading the chain
    The chain is required to provide proper root CA files on the nodes.

--- tools/vault/load-signed-intermediates.sh
+++ tools/vault/load-signed-intermediates.sh
@@ -4,7 +4,22 @@ cluster="$1"
 # shellcheck source=tools/vault/lib.sh
 . "$(dirname "$0")/lib.sh"
 
-vault write "$k8s_pki_path/intermediate/set-signed" certificate=@k8s-cluster.crt
-vault write "$etcd_pki_path/intermediate/set-signed" certificate=@k8s-etcd.crt
-vault write "$k8s_front_proxy_pki_path/intermediate/set-signed" certificate=@k8s-front-proxy.crt
-vault write "$calico_pki_path/intermediate/set-signed" certificate=@k8s-calico.crt
+function import_cert {
+    local chainfile="$1"
+    local pkipath="$2"
+
+    response="$(vault write -format=json "$pkipath/intermediate/set-signed" certificate="@$chainfile")"
+    issuer="$(jq -r '.data.imported_issuers[0]' <<<"$response")"
+    # If this is a no-op update to an existing issuer, the ID is not included
+    # in the response. Hence, we can't init it, but we probably also don't have
+    # to (because it has been imported before).
+    if [ "$issuer" != 'null' ]; then
+        vault write "$pkipath/issuer/$issuer" leaf_not_after_behavior=truncate
+        vault write "$pkipath/config/issuers" default="$issuer" >/dev/null
+    fi
+}
+
+import_cert k8s-cluster.fullchain.pem "$k8s_pki_path"
+import_cert k8s-front-proxy.fullchain.pem "$k8s_front_proxy_pki_path"
+import_cert k8s-calico.fullchain.pem "$calico_pki_path"
+import_cert k8s-etcd.fullchain.pem "$etcd_pki_path"
