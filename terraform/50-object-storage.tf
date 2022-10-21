87ecdcd796a64f0f63202694011f37c23d3fc498
Yannic Ahrens <yannic.ahrens@cloudandheat.com>
2022-10-21 16:26:02 +0200

    tf/thanos: skip template instantiation
    `monitoring_thanos_objectstorage_container_name` isn't read anywhere in
    stage3 and not passed to stage4. Let's drop it.

--- terraform/50-object-storage.tf
+++ terraform/50-object-storage.tf
@@ -3,17 +3,3 @@ resource "openstack_objectstorage_container_v1" "thanos_data" {
   count = var.monitoring_use_thanos ? 1 : 0
   force_destroy = var.thanos_delete_container
 }
-
-data "template_file" "final_all" {
-  template = file("${path.module}/templates/object_storage.tpl")
-  vars = {
-    monitoring_thanos_objectstorage_container_name = var.monitoring_use_thanos ? openstack_objectstorage_container_v1.thanos_data[0].name : ""
-  }
-}
-
-resource "local_file" "final_all" {
-  count = var.monitoring_use_thanos ? 1 : 0
-  content         = data.template_file.final_all.rendered
-  filename        = "../../inventory/03_k8s_base/group_vars/all/terraform_object-storage.yaml"
-  file_permission = 0640
-}
