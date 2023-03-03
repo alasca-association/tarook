3e5e7336459f4cccae3e981d92f105a2224a5822
Steve Starke <steve.starke@cloudandheat.com>
2023-03-03 09:39:14 +0100

    Export KUBECONFIG for stage3
    This allows us to interact with the Kubernetes API (after it has been
    spawned) from the orchestration (local-) host. This is handy and wanted,
    because we can then avoid the delegation to master[0] for such tasks.
    master[0] may not be available which would lead to failures of these tasks.
    
    We should find all occasions of tasks which interact with the Kubernetes
    API and are delegated to master[0] and switch the delegation to
    localhost instead.

--- actions/apply-stage3.sh
+++ actions/apply-stage3.sh
@@ -11,6 +11,7 @@ python3 "$actions_dir/update_inventory.py"
 # Bring the wireguard interface up if configured so
 "$actions_dir/wg-up.sh"
 
+export KUBECONFIG="$cluster_repository/inventory/.etc/admin.conf"
 cd "$ansible_k8s_base_playbook"
 ansible-galaxy install -r "$ansible_directory/requirements.yaml"
 ansible_playbook -i "$ansible_inventoryfile_03" install_k8s_base.yaml "$@"
