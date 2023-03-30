fdafe4a780ea4a7db9957ae99a2164d8a5a7cf9c
Dag Dammann <dag.dammann@cloudandheat.com>
2023-03-30 11:01:43 +0200

    Remove the wireguard connection command from stage 3

--- actions/apply-stage3.sh
+++ actions/apply-stage3.sh
@@ -8,9 +8,6 @@ python3 "$actions_dir/update_inventory.py"
 # shellcheck source=actions/lib.sh
 . "$actions_dir/lib.sh"
 
-# Bring the wireguard interface up if configured so
-"$actions_dir/wg-up.sh"
-
 export KUBECONFIG="$cluster_repository/inventory/.etc/admin.conf"
 cd "$ansible_k8s_base_playbook"
 ansible-galaxy install -r "$ansible_directory/requirements.yaml"
