11ed4d7468d4f556ba3d7fd91903cc66fdab6e2f
Dag Dammann <dag.dammann@cloudandheat.com>
2023-03-30 11:52:55 +0200

    Remove the custom stages concept

--- actions/apply.sh
+++ actions/apply.sh
@@ -5,8 +5,4 @@ actions_dir="$(dirname "$0")"
 "$actions_dir/apply-stage2.sh"
 "$actions_dir/apply-stage3.sh"
 
-if [ "${K8S_CUSTOM_STAGE_USAGE:-false}" == 'true' ]; then
-  "$actions_dir/apply-custom.sh"
-fi
-
 "$actions_dir/test.sh"
