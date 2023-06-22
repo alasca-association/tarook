a805ee501d132d630ae721e9fcccf5ba7be26214
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2023-06-22 14:53:14 +0200

    Add flake.nix
    Nix is a declarative package manager which powers NixOS but can also be
    installed as an additional package manager on any other distribution.
    This commit adds a flake.nix which references all necessary
    dependencies which are locked to specific versions.
    
    Together with poetry (which is also installed by the flake), this
    allows for 100% reproducible and (should you chose to) pure
    environments shared between all users.

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = [
+          pkgs.openstackclient
+          pkgs.k9s
+          pkgs.kubernetes-helm
+          pkgs.kubectl
+          pkgs.jq
+          pkgs.moreutils
+          pkgs.jsonnet
+          pkgs.terraform
+          pkgs.vault
+          pkgs.wireguard-tools
+          pkgs.poetry
+        ];
+      };
+    });
+}
