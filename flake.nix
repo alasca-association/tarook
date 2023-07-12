fb180f1a361408630da64c387c4e3feafa58bd1c
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2023-07-12 15:55:52 +0200

    flake.nix: Pin poetry to v1.3.2
    This is done to keep it in sync with the version used in the debian 12
    CI image.

--- flake.nix
+++ flake.nix
@@ -1,10 +1,14 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  # pin poetry 1.3.2
+  # TODO: keep version in sync with CI image
+  inputs.nixpkgs-poetry.url = "github:NixOS/nixpkgs/8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8";
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, nixpkgs-poetry, ... }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
+      poetry_v1_3_2 = nixpkgs-poetry.legacyPackages.${system}.poetry;
     in {
       devShell = pkgs.mkShell {
         nativeBuildInputs = [ pkgs.bashInteractive ];
@@ -19,7 +23,7 @@
           pkgs.terraform
           pkgs.vault
           pkgs.wireguard-tools
-          pkgs.poetry
+          poetry_v1_3_2
         ];
       };
     });
