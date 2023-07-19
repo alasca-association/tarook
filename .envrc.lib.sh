05e87c4a732cd2e479ec4e8fff22b2d8b20b3ed1
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2023-07-19 15:02:22 +0200

    Source current nix_direnv if using flake
    This speeds up entering the environment significantly

--- .envrc.lib.sh
+++ .envrc.lib.sh
@@ -35,6 +35,9 @@ use_flake_if_nix() {
   if has nix; then
     if [ -z "$(comm -13 <(nix show-config | grep -Po 'experimental-features = \K(.*)' | tr " " "\n" |  sort) <(echo "flakes nix-command" | tr " " "\n"))" ];
     then
+      if ! has nix_direnv_version || ! nix_direnv_version 2.3.0; then
+        source_url "https://raw.githubusercontent.com/nix-community/nix-direnv/2.3.0/direnvrc" "sha256-Dmd+j63L84wuzgyjITIfSxSD57Tx7v51DMxVZOsiUD8="
+      fi
       use flake "$flake_dir"
     else
       echo "Not loading flake. Nix is installed, but flakes are not enabled."
