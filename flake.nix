afe866d39e57cb4f863de5b0508ea9e029f38866
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2023-08-22 20:42:39 +0200

    add krew plugins

--- flake.nix
+++ flake.nix
@@ -4,10 +4,20 @@
   # pin poetry 1.3.2
   # TODO: keep version in sync with CI image
   inputs.nixpkgs-poetry.url = "github:NixOS/nixpkgs/8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8";
+  inputs.krew2nix.url = "github:Lykos153/krew2nix";
+  inputs.krew2nix.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, flake-utils, nixpkgs-poetry, ... }:
+  outputs = { self, nixpkgs, flake-utils, nixpkgs-poetry, krew2nix, ... }@inputs:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+                (final: prev: {
+                    kubectl = krew2nix.outputs.packages.${system}.kubectl;
+                })
+
+        ];
+      };
       poetry_v1_3_2 = nixpkgs-poetry.legacyPackages.${system}.poetry;
     in {
       devShell = pkgs.mkShell {
@@ -16,7 +26,12 @@
           pkgs.openstackclient
           pkgs.k9s
           pkgs.kubernetes-helm
-          pkgs.kubectl
+          (pkgs.kubectl.withKrewPlugins (plugins: with plugins; [
+            node-shell
+            get-all
+            example
+            rook-ceph
+          ]))
           pkgs.jq
           pkgs.moreutils
           pkgs.jsonnet
