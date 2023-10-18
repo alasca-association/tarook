f25ce4cb8f2307022eb9d35dd8d8229851431a43
Steve Starke <steve.starke@cloudandheat.com>
2023-10-18 15:14:40 +0200

    core-split: Rework cluster health verification
    Due to the separation of k8s-core and k8s-supplements, we must make the
    cluster health verification more modular. As the cluster health
    verification mostly happens during Kubernetes upgrades and these are
    mainly part of k8s-core, but the health checks are currently mostly rook
    focused and rook is part of k8s-supplements, a mechanism had to be
    implemented such that the k8s-supplements-healh-verifications can be
    triggered from within k8s-core without knowledge about what has to be
    verified. This has been realized by creating verify-* action scripts and
    moving cluster-health-tasks into own playbooks which can be triggerd by
    these action scripts.

---
+++ actions/verify-cluster-health.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -euo pipefail
+actions_dir="$(dirname "$0")"
+
+# shellcheck source=actions/lib.sh
+. "$actions_dir/lib.sh"
+
+while getopts s flag
+do
+    case "${flag}" in
+        s)
+            k8s_skip_upgrade_checks=true
+            ;;
+        *)
+            echo "Unknown flag passed: '${flag}'" >&2
+            exit 1
+            ;;
+    esac
+done
+
+shift $(( OPTIND - 1 ))
+
+pushd "$ansible_k8s_supplements_dir"
+# Include k8s-core roles
+ANSIBLE_ROLES_PATH="$ansible_k8s_core_dir/roles:$ansible_k8s_supplements_dir/roles" \
+  ansible_playbook -i "$ansible_inventory_host_file" \
+  -e "k8s_skip_upgrade_checks=${k8s_skip_upgrade_checks:-false}" \
+  verify-cluster-health.yaml "$@"
+popd
