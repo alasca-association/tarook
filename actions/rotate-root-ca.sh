f0de72025295ffdeac722a2b14343aa04670b6a4
Steve Starke <steve.starke@cloudandheat.com>
2023-12-06 16:15:27 +0100

    Implement & enforce modes for CA rotation
    This gives the CA rotation action script two modes of which one must be
    selected. In the first mode an additional issuer "next" is configured
    and two CAs are considered valid by the k8s cluster components. Certs
    get renewed by force and issued by the next issuer.
    In the second mode, the CA rotation can be completed by dropping
    basically invalidating the previous CA and allowing only the new one to
    be considered valid. All certs and kubeconfigs will get renewed again as
    we must restart all components anyway to make changes persistent (so
    that they do not trust the previous CA anymore). We have to enforce the
    renewal as the issued certs may still be in the valid timerange and
    therefore would not get renewed which would skip restarting the
    components.

--- actions/rotate-root-ca.sh
+++ actions/rotate-root-ca.sh
@@ -2,6 +2,43 @@
 set -euo pipefail
 actions_dir="$(dirname "$0")"
 
+helpf() {
+  printf "Usage: %s [-n][-c]\n\n" "$0" 1>&2;
+  printf "Either [-n] OR [-c] must be specified.\n";
+  printf "[-n] Start CA rotation by appending additional issuer 'next'.\n";
+  printf "[-c] Complete CA rotation and apply only the default issuer.\n\n";
+  exit 2
+}
+
+while getopts "nc" flag
+do
+    case "${flag}" in
+        n)
+            next_issuer=true
+            ;;
+        c)
+            complete_rotation=true
+            ;;
+        *)
+            echo "Unknown flag passed: '${flag}'" >&2
+            helpf
+            ;;
+    esac
+done
+shift $(( OPTIND - 1 ))
+
+if [ "$#" != 0 ]; then
+  helpf
+fi
+
+if [ -z "${next_issuer:-}" ] && [ -z "${complete_rotation:-}" ]; then
+  helpf
+fi
+
+if [ -n "${next_issuer:-}" ] && [ -n "${complete_rotation:-}" ]; then
+  helpf
+fi
+
 # Ensure that the latest config is deployed to the inventory
 python3 "$actions_dir/update_inventory.py"
 
@@ -16,4 +53,7 @@ require_disruption
 export KUBECONFIG="$cluster_repository/inventory/.etc/admin.conf"
 cd "$ansible_k8s_base_playbook"
 ansible-galaxy install -r "$ansible_directory/requirements.yaml"
-ansible_playbook -i "$ansible_inventoryfile_03" rotate_root_ca.yaml "$@"
+
+ansible_playbook -i "$ansible_inventoryfile_03" \
+  -e "append_next_issuer=${next_issuer:-false}" \
+  rotate_root_ca.yaml "$@"
