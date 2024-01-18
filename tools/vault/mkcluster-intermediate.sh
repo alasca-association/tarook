141a794e3ff2ac8154ff8643cde87d3a56b2c22a
Steve Starke <steve.starke@cloudandheat.com>
2024-01-18 16:35:41 +0100

    Add missing IPSec vault migration
    This has been overseen in !949

--- tools/vault/mkcluster-intermediate.sh
+++ tools/vault/mkcluster-intermediate.sh
@@ -23,3 +23,7 @@ echo "NOTE: CSRs have been written to k8s-{cluster,etcd,front-proxy,calico}.csr.
 echo "-----------------------------------------------"
 echo "Trying to importing etcd backup credentials ..."
 import_etcd_backup_config
+
+echo "-----------------------------------------------"
+echo "Trying to import IPSec EAP PSK ..."
+import_ipsec_eap_psk
