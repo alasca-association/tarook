c58367bf54b11809c63a13d9cdeb0a1e9e17fae2
Steve Starke <steve.starke@cloudandheat.com>
2024-03-01 12:00:33 +0100

    core-split: Adjust CA rotation
    Adjust paths and references for the CA rotation.

--- actions/rotate-root-ca.sh
+++ actions/rotate-root-ca.sh
@@ -47,19 +47,25 @@ python3 "$actions_dir/update_inventory.py"
 
 require_disruption
 
+require_vault_token
+
+install_prerequisites
+
 # Bring the wireguard interface up if configured so
 "$actions_dir/wg-up.sh"
 
-export KUBECONFIG="$cluster_repository/inventory/.etc/admin.conf"
-cd "$ansible_k8s_base_playbook"
-ansible-galaxy install -r "$ansible_directory/requirements.yaml"
-
-ansible_playbook -i "$ansible_inventoryfile_03" \
+pushd "$ansible_k8s_core_dir"
+ansible_playbook -i "$ansible_inventory_host_file" \
   -e "append_next_issuer=${next_issuer:-false}" \
   -e "complete_rotation=${complete_rotation:-false}" \
-  rotate_root_ca.yaml "$@"
+  rotate-root-ca.yaml "$@"
+popd
 
-cd "$ansible_k8s_sl_playbook"
-ansible_playbook -i "inventory/default.yaml" \
-  -e "ksl_vars_directory=$ansible_k8s_sl_vars_base" \
-  rotate_root_ca.yaml
+pushd "$ansible_k8s_supplements_dir"
+# Include k8s-core roles
+ANSIBLE_ROLES_PATH="$ansible_k8s_core_dir/roles:$ansible_k8s_supplements_dir/roles" \
+  ansible_playbook -i "$ansible_inventory_host_file" \
+  -e "ansible_k8s_core_dir=$ansible_k8s_core_dir" \
+  -e "k8s_skip_upgrade_checks=${k8s_skip_upgrade_checks:-false}" \
+  rotate-root-ca.yaml "$@"
+popd
