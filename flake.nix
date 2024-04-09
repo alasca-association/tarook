0bd3cca8972f97d40d7b96acf612d87ad6962a25
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-04-09 16:54:26 +0200

    flake.nix: use flake-parts
    Flake Parts[1] brings the NixOS module system to general purpose flakes.
    It simplifies handling multiple architectures, improves composability
    (which helps solve #616 and #646) and lays the groundwork for all future
    features implemented using Nix (eg. !1265).
    
    [1] https://flake.parts

--- flake.nix
+++ flake.nix
@@ -1,40 +1,48 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
 
-  outputs = {
+  outputs = inputs @ {
     self,
     nixpkgs,
-    flake-utils,
+    flake-parts,
     ...
   }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfreePredicate = pkg:
-          builtins.elem (nixpkgs.outputs.lib.getName pkg) [
-            "terraform"
-            "vault"
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+      ];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: {
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfreePredicate = pkg:
+            builtins.elem (nixpkgs.outputs.lib.getName pkg) [
+              "terraform"
+              "vault"
+            ];
+        };
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [pkgs.bashInteractive];
+          buildInputs = [
+            pkgs.openstackclient
+            pkgs.k9s
+            pkgs.kubernetes-helm
+            pkgs.kubectl
+            pkgs.jq
+            pkgs.moreutils
+            pkgs.terraform
+            pkgs.vault
+            pkgs.openssl
+            pkgs.wireguard-tools
+            pkgs.poetry
           ];
-      };
-    in {
-      devShell = pkgs.mkShell {
-        nativeBuildInputs = [pkgs.bashInteractive];
-        buildInputs = [
-          pkgs.openstackclient
-          pkgs.k9s
-          pkgs.kubernetes-helm
-          pkgs.kubectl
-          pkgs.jq
-          pkgs.moreutils
-          pkgs.terraform
-          pkgs.vault
-          pkgs.openssl
-          pkgs.wireguard-tools
-          pkgs.poetry
-        ];
-      };
+        };
 
-      formatter = pkgs.alejandra;
-    });
+        formatter = pkgs.alejandra;
+      };
+    };
 }
