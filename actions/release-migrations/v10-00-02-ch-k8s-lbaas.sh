84ce538348c882f8774ec2f3507848b2d2981af3
Theresa Schüttig <theresa.schuettig@cloudandheat.com>
2024-06-03 15:15:11 +0200

    Add LBaaS configuration for OVN compatibility
    The fixed IP of the VIP port and the port IDs of the gateway nodes need
    to be stored inside the controller-config.toml of the ch-k8s-lbaas so
    the VIP port IP and the fixed IP addresses of L3 ports can be added to
    the allowed address pairs of each gateway node port by the ch-k8s-lbaas.
    
    In addition, it is important to also allow "0.0.0.0/0" if IPv4 is
    enabled and "::/0" if IPv6 is enabled, because otherwise the agents
    would drop these from the OpenStack ports which would restrict traffic
    and leads to connectivity issues.

---
+++ actions/release-migrations/v10-00-02-ch-k8s-lbaas.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# NOTE: This migration must be run before port security is enabled by Terraform
+
+set -euo pipefail
+actions_dir="$(dirname "$0")/.."
+
+# shellcheck source=actions/lib.sh
+. "$actions_dir/lib.sh"
+
+# Let ch-k8s-lbaas version >= 0.8.0 be enforced by Nix
+run "$actions_dir/update-inventory.sh"
+
+notef "Triggering install-ch-k8s-lbaas.yaml playbook to update ch-k8s-lbaas."
+run "$actions_dir/apply-k8s-supplements.sh" install-ch-k8s-lbaas.yaml
