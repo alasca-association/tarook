21c80f5c45762faf7f9420a1b0114e2221336e98
Steve Starke <steve.starke@cloudandheat.com>
2024-06-21 10:22:15 +0000

    Adjust super admin kubeconfig handling
    Add support to the k8s-login procedure to generate a super admin
    kubeconfig. Such scoped kubeconfigs are necessary for kubeadm to be
    able to initialize a cluster so we can't restrict the vault policies
    for the control plane here and must ensure a super admin kubeconfig is
    around when initializing the Kubernetes itself. Afterwards, we can
    remove it from the nodes for security reasons.
    
    Due to the modulartiy and the scopes of k8s-core and k8s-supplements, we
    must ensure adjusting the scope of the kubeconfigs on the control plane
    nodes is handled inside of k8s-core.

--- actions/k8s-login.sh
+++ actions/k8s-login.sh
@@ -7,9 +7,33 @@ actions_dir="$(dirname "$0")"
 
 require_vault_token
 
+while getopts s flag
+do
+    case "${flag}" in
+        s)
+            super_admin=true
+            ;;
+        *)
+            echo "Unknown flag passed: '${flag}'" >&2
+            exit 1
+            ;;
+    esac
+done
+
+shift $(( OPTIND - 1 ))
+[[ "${0}" == "--" ]] && shift
+
+arg_num=0
+if [ "$#" -ne "$arg_num" ]; then
+    echo "ERROR: Expecting $arg_num argument(s), but $# were given" >&2
+    echo >&2
+    exit 2
+fi
+
 pushd "$ansible_k8s_core_dir"
 # Include k8s-core roles
 ANSIBLE_ROLES_PATH="$ansible_k8s_core_dir/roles" \
   ansible_playbook -i "$ansible_inventory_host_file" \
+  -e "super_admin=${super_admin:-false}" \
   k8s-login.yaml "$@"
 popd
--- tools/vault/k8s-login.sh
+++ tools/vault/k8s-login.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 set -euo pipefail
-actions_dir="$(realpath "$(dirname "$0")")"
+actions_dir="$(realpath "$(dirname "$0")")/../../actions"
 # shellcheck source=tools/vault/lib.sh
 . "$(dirname "$0")/lib.sh"
+
+
+while getopts s flag
+do
+    case "${flag}" in
+        s)
+            super_admin=true
+            ;;
+        *)
+            echo "Unknown flag passed: '${flag}'" >&2
+            exit 1
+            ;;
+    esac
+done
+
+shift $(( OPTIND - 1 ))
+[[ "${1}" == "--" ]] && shift
 
 arg_num=1
 if [ "$#" -ne "$arg_num" ]; then
@@ -17,13 +34,12 @@ kubernetes_server="$1"
 username="vault:$(vault token lookup -format=json | jq -r .data.path)"
 kubernetes_version="$(tomlq --raw-output '.kubernetes.version // error("unset")' config/config.toml)"
 
-if [ "$("$actions_dir/../../actions/helpers/semver2.sh" "$kubernetes_version" "1.29")" -lt 0 ]; then
-    credentials=$(vault write -format=json yaook/"$cluster"/k8s-pki/issue/any-master common_name="$username" ttl=192h)  # 8 days
-    jq --arg "username" "$username" --arg "k8s_server" "$kubernetes_server" '{"apiVersion": "v1", "clusters": [{"cluster": {"certificate-authority-data": .data.issuing_ca | @base64, "server": $k8s_server}, "name": "kubernetes"}], "contexts": [{"context": {"cluster": "kubernetes", "user": $username}, "name": "\($username)@kubernetes"}], "current-context": "\($username)@kubernetes", "kind": "Config", "preferences": {}, "users": [{"name": $username, "user": {"client-certificate-data": .data.certificate | @base64, "client-key-data": .data.private_key | @base64}}]}' <<<"$credentials"
-elif [ "$("$actions_dir/../../actions/helpers/semver2.sh" "$kubernetes_version" "1.29")" -gt 0 ]; then
+if [ "${super_admin:-false}" == false ]; then
     credentials=$(vault write -format=json yaook/"$cluster"/k8s-pki/issue/any-cluster-admin common_name="$username" ttl=192h)  # 8 days
-    jq --arg "username" "$username" --arg "k8s_server" "$kubernetes_server" '{"apiVersion": "v1", "clusters": [{"cluster": {"certificate-authority-data": .data.issuing_ca | @base64, "server": $k8s_server}, "name": "kubernetes"}], "contexts": [{"context": {"cluster": "kubernetes", "user": $username}, "name": "\($username)@kubernetes"}], "current-context": "\($username)@kubernetes", "kind": "Config", "preferences": {}, "users": [{"name": $username, "user": {"client-certificate-data": .data.certificate | @base64, "client-key-data": .data.private_key | @base64}}]}' <<<"$credentials"
-else
-    errorf 'Version comparison failed'
-    exit 2
 fi
+# For Kubernetes <= v1.29 we must use any-master
+# Drop the OR-condition along with Kubernetes v1.28
+if [ "${super_admin:-false}" == true ] || [ "$("$actions_dir/helpers/semver2.sh" "$kubernetes_version" "1.29")" -lt 0 ]; then
+    credentials=$(vault write -format=json yaook/"$cluster"/k8s-pki/issue/any-master common_name="$username" ttl=192h)  # 8 days
+fi
+jq --arg "username" "$username" --arg "k8s_server" "$kubernetes_server" '{"apiVersion": "v1", "clusters": [{"cluster": {"certificate-authority-data": .data.issuing_ca | @base64, "server": $k8s_server}, "name": "kubernetes"}], "contexts": [{"context": {"cluster": "kubernetes", "user": $username}, "name": "\($username)@kubernetes"}], "current-context": "\($username)@kubernetes", "kind": "Config", "preferences": {}, "users": [{"name": $username, "user": {"client-certificate-data": .data.certificate | @base64, "client-key-data": .data.private_key | @base64}}]}' <<<"$credentials"
