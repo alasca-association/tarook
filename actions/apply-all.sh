4cd964d61d891519442b9d85258ecbe8ee1df08b
Steve Starke <steve.starke@cloudandheat.com>
2024-06-26 14:21:33 +0000

    LCM adjustments

--- actions/apply-all.sh
+++ actions/apply-all.sh
@@ -7,17 +7,15 @@ actions_dir="$(dirname "$0")"
 
 check_venv
 
-kubectl get nodes
+# # Invoke Terraform, if configured
+# if [ "${TF_USAGE:-true}" == 'true' ]; then
+#   run "$actions_dir/apply-terraform.sh"
+# fi
 
-# Invoke Terraform, if configured
-if [ "${TF_USAGE:-true}" == 'true' ]; then
-  run "$actions_dir/apply-terraform.sh"
-fi
-
-# Prepare Gateways, if configured
-if [ "${TF_USAGE:-true}" == 'true' ]; then
-  run "$actions_dir/apply-prepare-gw.sh"
-fi
+# # Prepare Gateways, if configured
+# if [ "${TF_USAGE:-true}" == 'true' ]; then
+#   run "$actions_dir/apply-prepare-gw.sh"
+# fi
 
 # Invoke whole k8s-supplements (including k8s-core)
 run "$actions_dir/apply-k8s-supplements.sh"
