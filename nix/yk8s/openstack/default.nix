9d37e945e4d407db6c645ae9e92f31fd1874f273
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-08-22 16:03:53 +0200

    Implement support for multiple interfaces

--- nix/yk8s/openstack/default.nix
+++ nix/yk8s/openstack/default.nix
@@ -667,6 +667,8 @@ in {
         networking_floating_ip = config.yk8s.terraform.outputs.networking_floating_ip.value;
         networking_fixed_ip = config.yk8s.terraform.outputs.networking_fixed_ip.value or null;
         networking_fixed_ip_v6 = config.yk8s.terraform.outputs.networking_fixed_ip_v6.value or null;
+        public_fixed_ip = config.yk8s.terraform.outputs.networking_fixed_ip.value or null;
+        public_fixed_ip_v6 = config.yk8s.terraform.outputs.networking_fixed_ip_v6.value or null;
         ansible_hosts = let
           nodeConfigs =
             lib.mapAttrs (
