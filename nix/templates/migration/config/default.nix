777bc80d03286aabf533c8ac5ed651c7773e7c1d
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-09-06 16:20:10 +0200

    Drop C&H specific Terraform defaults
    Some input variables used default values that are highly specific to the
    underlying Openstack cloud, such as image and flavor names. It is not
    useful to preset them.
    
    Thus, Cloud&Heat specific default have been moved from the Terraform
    default values to the template file where they act as both an example
    and a way for company users to quickly set up a cluster.

--- nix/templates/migration/config/default.nix
+++ nix/templates/migration/config/default.nix
@@ -11,5 +11,20 @@ in
   recursiveUpdate {
     terraform = {
       cluster_name = "managed-k8s";
+
+      master_defaults = {
+        flavor = "M";
+        image = "Ubuntu 22.04 LTS x64";
+      };
+      worker_defaults = {
+        flavor = "M";
+        image = "Ubuntu 22.04 LTS x64";
+      };
+      gateway_defaults = {
+        image = "Debian 12 (bookworm)";
+        flavor = "XS";
+      };
+
+      public_network = "shared-public-IPv4";
     };
   } (yk8s-lib.importTOML ./config.toml)
