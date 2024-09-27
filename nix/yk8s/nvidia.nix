e7f5f2afdfb927b83cd30d9451849dd89a210943
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-09-27 22:54:19 +0200

    Nixify nvidia helm values

--- nix/yk8s/nvidia.nix
+++ nix/yk8s/nvidia.nix
@@ -5,9 +5,12 @@
   ...
 }: let
   cfg = config.yk8s.nvidia;
+  modules-lib = import ./lib/modules.nix {inherit lib;};
   inherit (lib) mkOption types;
   inherit (yk8s-lib) mkTopSection mkGroupVarsFile;
+  inherit (modules-lib) mkHelmValuesModule;
 in {
+  imports = [(mkHelmValuesModule "nvidia" "device_plugin")];
   options.yk8s.nvidia = mkTopSection {
     vgpu = {
       driver_blob_url = mkOption {
@@ -24,15 +27,49 @@ in {
       };
     };
   };
+
+  config.yk8s.nvidia.device_plugin_default_values = {
+    nodeSelector = {
+      "k8s.yaook.cloud/gpu-node" = "true";
+    };
+    tolerations = [
+      {
+        key = "";
+        operator = "Exists";
+      }
+    ];
+
+    # Subcharts
+    # https://github.com/NVIDIA/gpu-operator/tree/master/deployments/gpu-operator/charts/node-feature-discovery
+    nfd = {
+      worker = {
+        tolerations = [
+          {
+            key = "";
+            operator = "Exists";
+          }
+        ];
+        nodeSelector = {
+          "k8s.yaook.cloud/gpu-node" = "true";
+        };
+      };
+    };
+
+    # https://github.com/NVIDIA/gpu-feature-discovery
+    gfd = {
+      enabled = true;
+    };
+  };
+
   config.yk8s._inventory_packages = [
     (mkGroupVarsFile {
       inherit cfg;
       ansible_prefix = "nvidia_";
       inventory_path = "all/nvidia.yaml";
       transformations = [
-        (cfg:
+        (c:
           if config.yk8s.kubernetes.virtualize_gpu
-          then cfg
+          then c
           else {})
       ];
     })
