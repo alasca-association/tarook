a6d8124c3b2bda45cc376b55ca5fc691d5527248
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-09-28 00:34:39 +0200

    Add mkTolerations and mkAffinity

--- nix/yk8s/lib/default.nix
+++ nix/yk8s/lib/default.nix
@@ -82,4 +82,59 @@
     name = lib.strings.removeSuffix ".yaml" file;
     value = importYAML "${dir}/${file}";
   }) (builtins.readDir dir));
+
+  mkTolerations = scheduling_key:
+    lib.optionals (scheduling_key != null) [
+      {
+        "key" = scheduling_key;
+        "operator" = "Exists";
+        "effect" = "NoSchedule";
+      }
+      {
+        "key" = scheduling_key;
+        "operator" = "Exists";
+        "effect" = "NoExecute";
+      }
+    ];
+
+  mkAffinity = {
+    scheduling_key,
+    pod_affinity_key ? null,
+    pod_affinity_operator ? "Exists",
+    pod_affinity_values ? [],
+  }:
+    (lib.optionalAttrs (scheduling_key != null) {
+      "nodeAffinity" = {
+        "requiredDuringSchedulingIgnoredDuringExecution" = {
+          "nodeSelectorTerms" = [
+            {
+              "matchExpressions" = [
+                {
+                  "key" = scheduling_key;
+                  "operator" = "Exists";
+                }
+              ];
+            }
+          ];
+        };
+      };
+    })
+    // (lib.optionalAttrs (pod_affinity_key != null) {
+      "podAffinity" = {
+        "requiredDuringSchedulingIgnoredDuringExecution" = [
+          {
+            "labelSelector" = {
+              "matchExpressions" = [
+                {
+                  "key" = pod_affinity_key;
+                  "operator" = pod_affinity_operator;
+                  "values" = pod_affinity_values;
+                }
+              ];
+            };
+            "topologyKey" = "kubernetes.io/hostname";
+          }
+        ];
+      };
+    });
 }
