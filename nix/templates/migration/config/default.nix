4860fb8ed2d77f22fd3e906264492f71d4a83acf
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-10-10 11:41:46 +0200

    Fix migration default.nix template

--- nix/templates/migration/config/default.nix
+++ nix/templates/migration/config/default.nix
@@ -8,12 +8,8 @@
   cfg = config.yk8s;
   inherit (lib.attrsets) recursiveUpdate;
 in
-  recursiveUpdate {
-    terraform = {
-      cluster_name = "managed-k8s";
-    };
-  } (
-    # importing an almost unchanged config.toml to prove we're building the same cluster as before
-    # TODO: migrate ci config to Nix in the future
+  recursiveUpdate (
     yk8s-lib.importTOML ./config.toml
-  )
+  ) {
+    # Add any overrides here
+  }
