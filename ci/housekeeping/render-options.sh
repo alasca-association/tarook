af9fa9d7f4682b422a687a97931cb8607ec5aa0b
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-11-04 17:22:08 +0100

    Add CI job to render options reference
    Since !1265, the option reference for the documentation is now rendered
    via Nix. Because they could be reproduces on-the-fly, the resulting RST
    files were not checked into git. However, implementing the rendering
    step with sphinx-multiversion has proved quite hard, thus it has been
    decided to check the rendered reference into git for now.
    
    This commits adds a new CI stage "housekeeping" containing a new job to
    render and commit the options reference if it has changed.

---
+++ ci/housekeeping/render-options.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+out="$(nix build --print-out-paths --no-link .#docsRST)"
+rsync -rL --delete --chmod 664 "$out/" docs/user/reference/options
+
+if [[ -n "${CI_COMMIT_BRANCH:-""}" ]]; then
+    git remote set-url origin "https://gitlab-ci-token:${PUSH_TOKEN}@${CI_SERVER_HOST}/${CI_PROJECT_PATH}.git"
+    git fetch origin
+    git checkout "${CI_COMMIT_BRANCH}"
+    git status
+    CHANGES=$(git diff "origin/${CI_COMMIT_BRANCH}" --name-only -- ./docs/user/reference/options | wc -l)
+elif [[ -n "${CI_MERGE_REQUEST_IID:-""}" ]]; then
+    git remote set-url origin "https://gitlab-ci-token:${PUSH_TOKEN}@${CI_SERVER_HOST}/${CI_MERGE_REQUEST_SOURCE_PROJECT_PATH}.git"
+    git fetch origin
+    git checkout "origin/${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}"
+    git status
+    CHANGES=$(git diff "origin/${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}" --name-only -- ./docs/user/reference/options | wc -l)
+else
+    echo "Automatically rendering docs is only available for MRs and on predefined branches. This pipeline runs for neither of them."
+    exit 1
+fi
+
+if [ "${CHANGES}" -gt 0 ]; then
+    pre-commit install
+    echo "committing"
+    git add ./docs/user/reference/options
+    pre-commit run
+    git add ./docs/user/reference/options
+    git commit -m "Update rendered docs" -m "job url: ${CI_JOB_URL}"
+    if [[ -n "${CI_COMMIT_BRANCH}" ]]; then
+        git push
+        exit 0
+    elif [[ -n "${CI_MERGE_REQUEST_IID}" ]]; then
+        git push origin HEAD:"${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}"
+        exit 0
+    fi
+else
+    echo "Rendered docs are up-to-date. Nothing to commit."
+fi
