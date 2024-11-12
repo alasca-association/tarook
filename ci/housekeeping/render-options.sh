6a8bafa3d947cc7e648703c0f16adfe0ba193874
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-11-12 11:07:18 +0100

    fixup! Add CI job to render options reference

--- ci/housekeeping/render-options.sh
+++ ci/housekeeping/render-options.sh
@@ -17,6 +17,12 @@ function diff_options() {
     run git add ./docs/user/reference/options
     run git status
     CHANGES=$(git diff "${branch}" --staged --name-only -- ./docs/user/reference/options | wc -l)
+    if [ "${CHANGES}" -gt 0 ]; then
+        run pre-commit install
+        run git commit -m "Update rendered docs" -m "job url: ${CI_JOB_URL}"
+    else
+        echo "Rendered docs are up-to-date. Nothing to commit."
+    fi
 }
 
 run nix build --no-link .#docsRST
@@ -25,23 +31,13 @@ run rsync -rL --delete --chmod 664 "$out/" docs/user/reference/options
 
 if [[ -n "${CI_COMMIT_BRANCH:-""}" ]]; then
     diff_options "https://gitlab-ci-token:${PUSH_TOKEN}@${CI_SERVER_HOST}/${CI_PROJECT_PATH}.git" "${CI_COMMIT_BRANCH}"
+    run git push
+    exit 0
 elif [[ -n "${CI_MERGE_REQUEST_IID:-""}" ]]; then
     diff_options "https://gitlab-ci-token:${PUSH_TOKEN}@${CI_SERVER_HOST}/${CI_MERGE_REQUEST_SOURCE_PROJECT_PATH}.git" "origin/${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}"
+    run git push origin HEAD:"${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}"
+    exit 0
 else
     echo "Automatically rendering docs is only available for MRs and on predefined branches. This pipeline runs for neither of them."
     exit 1
-fi
-
-if [ "${CHANGES}" -gt 0 ]; then
-    run pre-commit install
-    run git commit -m "Update rendered docs" -m "job url: ${CI_JOB_URL}"
-    if [[ -n "${CI_COMMIT_BRANCH}" ]]; then
-        run git push
-        exit 0
-    elif [[ -n "${CI_MERGE_REQUEST_IID}" ]]; then
-        run git push origin HEAD:"${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}"
-        exit 0
-    fi
-else
-    echo "Rendered docs are up-to-date. Nothing to commit."
 fi
