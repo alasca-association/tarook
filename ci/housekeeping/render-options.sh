7b4977381411875a78e75ddc04ef80f026d200c0
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-11-12 10:57:50 +0100

    fixup! Add CI job to render options reference

--- ci/housekeeping/render-options.sh
+++ ci/housekeeping/render-options.sh
@@ -5,22 +5,28 @@ actions_dir="$(dirname "$0")/../../actions"
 # shellcheck source=actions/lib.sh
 . "$actions_dir/lib.sh"
 
+function diff_options() {
+    url="$1"
+    branch="$2"
 
+    run git remote set-url origin "${url}"
+    run git fetch origin
+    run git checkout "${branch}"
+    run git add ./docs/user/reference/options
+    run pre-commit run
+    run git add ./docs/user/reference/options
+    run git status
+    CHANGES=$(git diff "${branch}" --staged --name-only -- ./docs/user/reference/options | wc -l)
+}
+
+run nix build --no-link .#docsRST
 out="$(nix build --print-out-paths --no-link .#docsRST)"
 run rsync -rL --delete --chmod 664 "$out/" docs/user/reference/options
 
 if [[ -n "${CI_COMMIT_BRANCH:-""}" ]]; then
-    run git remote set-url origin "https://gitlab-ci-token:${PUSH_TOKEN}@${CI_SERVER_HOST}/${CI_PROJECT_PATH}.git"
-    run git fetch origin
-    run git checkout "${CI_COMMIT_BRANCH}"
-    run git status
-    CHANGES=$(git diff "origin/${CI_COMMIT_BRANCH}" --name-only -- ./docs/user/reference/options | wc -l)
+    diff_options "https://gitlab-ci-token:${PUSH_TOKEN}@${CI_SERVER_HOST}/${CI_PROJECT_PATH}.git" "${CI_COMMIT_BRANCH}"
 elif [[ -n "${CI_MERGE_REQUEST_IID:-""}" ]]; then
-    run git remote set-url origin "https://gitlab-ci-token:${PUSH_TOKEN}@${CI_SERVER_HOST}/${CI_MERGE_REQUEST_SOURCE_PROJECT_PATH}.git"
-    run git fetch origin
-    run git checkout "origin/${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}"
-    run git status
-    CHANGES=$(git diff "origin/${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}" --name-only -- ./docs/user/reference/options | wc -l)
+    diff_options "https://gitlab-ci-token:${PUSH_TOKEN}@${CI_SERVER_HOST}/${CI_MERGE_REQUEST_SOURCE_PROJECT_PATH}.git" "origin/${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}"
 else
     echo "Automatically rendering docs is only available for MRs and on predefined branches. This pipeline runs for neither of them."
     exit 1
@@ -28,10 +34,6 @@ fi
 
 if [ "${CHANGES}" -gt 0 ]; then
     run pre-commit install
-    run echo "committing"
-    run git add ./docs/user/reference/options
-    run pre-commit run
-    run git add ./docs/user/reference/options
     run git commit -m "Update rendered docs" -m "job url: ${CI_JOB_URL}"
     if [[ -n "${CI_COMMIT_BRANCH}" ]]; then
         run git push
