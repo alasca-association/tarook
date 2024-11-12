3a3a2bdacbe05c06c1d405abfe2d9c1f19c65797
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-11-12 10:45:45 +0100

    fixup! Add CI job to render options reference

--- ci/housekeeping/render-options.sh
+++ ci/housekeeping/render-options.sh
@@ -7,19 +7,19 @@ actions_dir="$(dirname "$0")/../../actions"
 
 
 out="$(nix build --print-out-paths --no-link .#docsRST)"
-rsync -rL --delete --chmod 664 "$out/" docs/user/reference/options
+run rsync -rL --delete --chmod 664 "$out/" docs/user/reference/options
 
 if [[ -n "${CI_COMMIT_BRANCH:-""}" ]]; then
-    git remote set-url origin "https://gitlab-ci-token:${PUSH_TOKEN}@${CI_SERVER_HOST}/${CI_PROJECT_PATH}.git"
-    git fetch origin
-    git checkout "${CI_COMMIT_BRANCH}"
-    git status
+    run git remote set-url origin "https://gitlab-ci-token:${PUSH_TOKEN}@${CI_SERVER_HOST}/${CI_PROJECT_PATH}.git"
+    run git fetch origin
+    run git checkout "${CI_COMMIT_BRANCH}"
+    run git status
     CHANGES=$(git diff "origin/${CI_COMMIT_BRANCH}" --name-only -- ./docs/user/reference/options | wc -l)
 elif [[ -n "${CI_MERGE_REQUEST_IID:-""}" ]]; then
-    git remote set-url origin "https://gitlab-ci-token:${PUSH_TOKEN}@${CI_SERVER_HOST}/${CI_MERGE_REQUEST_SOURCE_PROJECT_PATH}.git"
-    git fetch origin
-    git checkout "origin/${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}"
-    git status
+    run git remote set-url origin "https://gitlab-ci-token:${PUSH_TOKEN}@${CI_SERVER_HOST}/${CI_MERGE_REQUEST_SOURCE_PROJECT_PATH}.git"
+    run git fetch origin
+    run git checkout "origin/${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}"
+    run git status
     CHANGES=$(git diff "origin/${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}" --name-only -- ./docs/user/reference/options | wc -l)
 else
     echo "Automatically rendering docs is only available for MRs and on predefined branches. This pipeline runs for neither of them."
@@ -27,17 +27,17 @@ else
 fi
 
 if [ "${CHANGES}" -gt 0 ]; then
-    pre-commit install
-    echo "committing"
-    git add ./docs/user/reference/options
-    pre-commit run
-    git add ./docs/user/reference/options
-    git commit -m "Update rendered docs" -m "job url: ${CI_JOB_URL}"
+    run pre-commit install
+    run echo "committing"
+    run git add ./docs/user/reference/options
+    run pre-commit run
+    run git add ./docs/user/reference/options
+    run git commit -m "Update rendered docs" -m "job url: ${CI_JOB_URL}"
     if [[ -n "${CI_COMMIT_BRANCH}" ]]; then
-        git push
+        run git push
         exit 0
     elif [[ -n "${CI_MERGE_REQUEST_IID}" ]]; then
-        git push origin HEAD:"${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}"
+        run git push origin HEAD:"${CI_MERGE_REQUEST_SOURCE_BRANCH_NAME}"
         exit 0
     fi
 else
