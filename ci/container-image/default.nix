438c4b982b2fc8b5aaed5942dd618cd5a99a4bbd
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-11-21 19:49:10 +0100

    Move CI package definitions to own module
    Defining Nix dependencies in a Flake module allows us to move the CI
    image definition into its own module as well, further cleaning up
    flake.nix and increasing separation of concerns.

--- ci/container-image/default.nix
+++ ci/container-image/default.nix
@@ -1,63 +1,76 @@
-{
-  pkgs,
-  yk8s-env-ci,
-  ...
-}: let
-  ciFiles = pkgs.stdenv.mkDerivation {
-    name = "ci-files";
-    src = ./.;
-    postInstall = ''
-      mkdir -p $out/root/.ssh
-      cp known_hosts $out/root/.ssh/known_hosts
-      cp gitconfig $out/root/.gitconfig
-      cp openrc_f1a.sh $out/root/openrc.sh
-    '';
+{flake-parts-lib, ...}: {
+  options = {
+    perSystem =
+      flake-parts-lib.mkPerSystemOption
+      ({
+        config,
+        pkgs,
+        lib,
+        ...
+      }: let
+        ciFiles = pkgs.stdenv.mkDerivation {
+          name = "ci-files";
+          src = ./.;
+          postInstall = ''
+            mkdir -p $out/root/.ssh
+            cp known_hosts $out/root/.ssh/known_hosts
+            cp gitconfig $out/root/.gitconfig
+            cp openrc_f1a.sh $out/root/openrc.sh
+          '';
+        };
+        userSetup = pkgs.stdenv.mkDerivation {
+          name = "user-setup";
+          src = ./.;
+          postInstall = ''
+            install -D -m622 user-setup/group $out/etc/group
+            install -D -m622 user-setup/passwd $out/etc/passwd
+            install -D -m620 user-setup/shadow $out/etc/shadow
+          '';
+        };
+        nixConfig = pkgs.stdenv.mkDerivation {
+          name = "nix-config";
+          src = ./.;
+          postInstall = ''
+            install -D nix.conf $out/etc/nix/nix.conf
+          '';
+        };
+        tmpdir = pkgs.runCommand "tmp-dir" {} "mkdir -p $out/tmp;";
+        container-image = {
+          name = "registry.gitlab.com/yaook/k8s/ci";
+          contents = pkgs.buildEnv {
+            name = "image-root";
+            paths = with pkgs; [
+              config.yk8s-env.environments.ci
+              bashInteractive
+              dockerTools.usrBinEnv
+              dockerTools.caCertificates
+              ciFiles
+              tmpdir
+              userSetup
+              nixConfig
+            ];
+          };
+          fakeRootCommands = ''
+            chmod 777 ${tmpdir}
+          '';
+          config = {
+            Cmd = [
+              "${pkgs.bashInteractive}/bin/bash"
+            ];
+            Env = [
+              "wg_private_key_file=/root/wg.key"
+              "wg_user=gitlab-ci-runner"
+              "TF_VAR_keypair=gitlab-ci-runner"
+              "HOME=/root"
+            ];
+          };
+          maxLayers = 120;
+        };
+      in {
+        packages = {
+          ciImage = pkgs.dockerTools.buildLayeredImage container-image;
+          streamCiImage = pkgs.writeShellScriptBin "stream-ci" (pkgs.dockerTools.streamLayeredImage container-image);
+        };
+      });
   };
-  userSetup = pkgs.stdenv.mkDerivation {
-    name = "user-setup";
-    src = ./.;
-    postInstall = ''
-      install -D -m622 user-setup/group $out/etc/group
-      install -D -m622 user-setup/passwd $out/etc/passwd
-      install -D -m620 user-setup/shadow $out/etc/shadow
-    '';
-  };
-  nixConfig = pkgs.stdenv.mkDerivation {
-    name = "nix-config";
-    src = ./.;
-    postInstall = ''
-      install -D nix.conf $out/etc/nix/nix.conf
-    '';
-  };
-  tmpdir = pkgs.runCommand "tmp-dir" {} "mkdir -p $out/tmp;";
-in {
-  name = "registry.gitlab.com/yaook/k8s/ci";
-  contents = pkgs.buildEnv {
-    name = "image-root";
-    paths = with pkgs; [
-      yk8s-env-ci
-      bashInteractive
-      dockerTools.usrBinEnv
-      dockerTools.caCertificates
-      ciFiles
-      tmpdir
-      userSetup
-      nixConfig
-    ];
-  };
-  fakeRootCommands = ''
-    chmod 777 ${tmpdir}
-  '';
-  config = {
-    Cmd = [
-      "${pkgs.bashInteractive}/bin/bash"
-    ];
-    Env = [
-      "wg_private_key_file=/root/wg.key"
-      "wg_user=gitlab-ci-runner"
-      "TF_VAR_keypair=gitlab-ci-runner"
-      "HOME=/root"
-    ];
-  };
-  maxLayers = 120;
 }
