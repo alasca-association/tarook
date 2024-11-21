ba0b7396a5a01c375c9662bdc670c2a3e97624f6
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2024-11-21 19:49:10 +0100

    Move CI package definitions to own module
    Defining Nix dependencies in a Flake module allows us to move the CI
    image definition into its own module as well, further cleaning up
    flake.nix and increasing separation of concerns.

--- flake.nix
+++ flake.nix
@@ -17,6 +17,7 @@
         ./nix/renderDocs.nix
         ./nix/yk8s-env.nix
         ./nix/templates
+        ./ci/container-image
       ];
       perSystem = {
         pkgs,
@@ -29,17 +30,10 @@
         _module.args.pkgs = import nixpkgs {
           inherit system;
         };
-        packages = let
-          container-image = import ./ci/container-image {
-            inherit lib pkgs;
-            inherit (config.packages) yk8s-env-ci;
-          };
-        in {
-          ciImage = pkgs.dockerTools.buildLayeredImage container-image;
-          streamCiImage = pkgs.writeShellScriptBin "stream-ci" (pkgs.dockerTools.streamLayeredImage container-image);
+        packages = {
           init = pkgs.writeShellApplication {
             name = "init-cluster-repo";
-            runtimeInputs = config.packages.yk8s-env-main;
+            runtimeInputs = config.yk8s-env.environments.main;
             text = ''
               if [[ -n "''${1:-""}" ]]; then
                 export MANAGED_K8S_LATEST_RELEASE=false
