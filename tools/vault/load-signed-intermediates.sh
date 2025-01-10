d14e908912a253d97de1a522cb49fb3dc81e1497
Jonas Schäfer <jonas.schaefer@cloudandheat.com>
2025-01-10 13:12:57 +0100

    Fix import of signed intermediates
    In 8153896, support for rotating the root CA of clusters with
    intermediate certificates was added. There, the signature of the
    `import_cert` function was added to include the issuer name.
    
    Unfortunately, the `load-signed-intermediates.sh` script was not updated
    for that.
    
    After reading the code, I'm reasonably confident that the correct issuer
    name for a freshly created cluster with signed intermediates is
    `current` (based on what the root CA rotation assigns for the default
    issuer).

--- tools/vault/load-signed-intermediates.sh
+++ tools/vault/load-signed-intermediates.sh
@@ -20,6 +20,6 @@ cluster="$(get_clustername)"
 # shellcheck source=tools/vault/lib.sh
 . "$(dirname "$0")/lib.sh"
 
-import_cert k8s-cluster.fullchain.pem "$k8s_pki_path"
-import_cert k8s-front-proxy.fullchain.pem "$k8s_front_proxy_pki_path"
-import_cert k8s-etcd.fullchain.pem "$etcd_pki_path"
+import_cert k8s-cluster.fullchain.pem "$k8s_pki_path" "current"
+import_cert k8s-front-proxy.fullchain.pem "$k8s_front_proxy_pki_path" "current"
+import_cert k8s-etcd.fullchain.pem "$etcd_pki_path" "current"
