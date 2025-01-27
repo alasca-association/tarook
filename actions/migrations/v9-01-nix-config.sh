f279c8932ef02187d379136106f35449b266b888
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2025-01-27 14:42:47 +0100

    Implement automatic config migration
    As of c0be6c50dac3dfe102777b777480205cb525bae7, the inventory is built
    from the configuration by Nix. Initially, the migration plan was to have
    the Nix file import the existing TOML file for minimal disruption.
    However, over the last three months, the Nix module has matured
    significantly on the devel branch.
    
    Moreover, the migration phase is the only opportunity for automatic
    conversion. Later, changes would require manual intervention. Hence,
    this commit extends the migration script to fully convert config.toml to
    default.nix.

--- actions/migrations/v9-01-nix-config.sh
+++ actions/migrations/v9-01-nix-config.sh
@@ -9,57 +9,97 @@ actions_dir="$(dirname "$0")/.."
 
 pushd "$cluster_repository" >/dev/null || exit 1
 
-echo "Rolling out Nix template"
-nix_output="$(nix flake init -t "${code_repository}#migration" 2>&1 || true)"
-nix_output="$(echo "$nix_output" | grep -vE 'error: Encountered [0-9]+ conflicts')" # We accept that some files may already exist
+config_default="config/default.nix"
+
+if [[ -f "inventory/yaook-k8s/hosts" ]] && \
+    ! tomlq --exit-status '.terraform | if has ("enabled") then .enabled else true end' "config/config.toml" >/dev/null;
+then
+    echo "Migrating manual hosts file..."
+    run git mv inventory/yaook-k8s/hosts config/hosts
+    echo Done.
+    echo
+fi
+
+if ! [[ -e "$config_default" ]]; then
+    echo "Converting config..."
+    cat <<EOF > "$config_default"
+{
+  pkgs,
+  lib,
+  yk8s-lib,
+  config,
+  ...
+}: let
+  cfg = config.yk8s;
+in {
+  yk8s = {
+      # A reference for all available options can be found at
+      # https://yaook.gitlab.io/k8s/devel/user/reference/options/index.html
+EOF
+    if [[ -e config/hosts ]]; then
+        echo "miscellaneous.hosts_file = ./hosts;" >> "$config_default"
+    fi
+    tomlq -s '.[0] * .[1]' "$actions_dir/migrations/v9-01-default.toml" "config/config.toml" \
+        | nix run github:cloudandheat/json2nix -- --strip-outer-bracket >> "$config_default"
+    printf "};}" >> "$config_default"
+    nix run nixpkgs#alejandra "$config_default" &>/dev/null
+    rm config/config.toml
+    echo "Done."
+    echo
+fi
+
+echo "Rolling out Nix template..."
+nix_output="$(nix flake init -t "${code_repository}#cluster-repo" 2>&1 || true)"
+nix_output="$(echo "$nix_output" | grep -vE 'error: Encountered [0-9]+ conflicts')" # We accept that some files already exist
 echo "$nix_output"
+echo
 if echo "$nix_output" | grep -q error; then
     errorf "Error during nix flake init"
     exit 1
 fi
 
-git add flake.nix config/default.nix
+run git add flake.nix config/default.nix
 
 if [[ -e "config/wireguard_ipam.toml" ]]; then
-    echo "Migrating wireguard state"
-    mkdir -p state/wireguard
-    git mv config/wireguard_ipam.toml state/wireguard/ipam.toml
+    echo "Migrating wireguard state..."
+    run mkdir -p state/wireguard
+    run git mv config/wireguard_ipam.toml state/wireguard/ipam.toml
+    echo "Done."
+    echo
 fi
 
 if [[ -d terraform ]]; then
-    echo "Migrating terraform state"
-    mkdir -p state
-    git mv terraform state/terraform
+    echo "Migrating terraform state..."
+    run mkdir -p state
+    run git mv terraform state/terraform
+    echo "Done."
+    echo
 fi
 
 tf_meta_state=state/terraform/.terraform/terraform.tfstate
 if [[ -e "$tf_meta_state" ]] && \
     jq -e '.backend.type == "local"' "$tf_meta_state" >/dev/null
 then
-    echo "Fixing terraform local backend path"
+    echo "Fixing terraform local backend path..."
     jq '.backend.config.path = "../../state/terraform/terraform.tfstate"' "$tf_meta_state" | sponge "$tf_meta_state"
+    echo "Done."
+    echo
 fi
 
 if [[ -d vault ]]; then
-    echo "Migrating vault state"
-    mkdir -p state
-    git mv vault state/vault
+    echo "Migrating vault state..."
+    run mkdir -p state
+    run git mv vault state/vault
+    echo "Done."
+    echo
 fi
 
-if [[ -f "inventory/yaook-k8s/hosts" ]] && \
-    ! tomlq --exit-status '.terraform | if has ("enabled") then .enabled else true end' "config/config.toml" >/dev/null;
-then
-    git mv inventory/yaook-k8s/hosts config/hosts
-    echo ""
-    echo "Attention:"
-    echo "Manual hosts file detected. Please add"
-    echo "miscellaneous.hosts_file = ./hosts;"
-    echo "to config/default.nix"
-fi
+run rm -rf inventory
 
-rm -rf inventory
-
-echo ""
+echo
 echo "Migration done!"
+
+echo "Building inventory..."
+run "$actions_dir/update-inventory.sh"
 
 popd >/dev/null || exit 1
