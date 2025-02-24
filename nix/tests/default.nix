99f41841f4887580e85ff91144bcfd8a628a4f12
Bruno Schubert <bruno.schubert@cloudandheat.com>
2025-02-24 22:11:17 +0100

    nix: Add unit testing fixture
    To enable unit testing of our Nix code this commit adds `nix/test.nix` as an
     entry point to the `nix/tests/` directory tree where unit tests for all the
     Nix modules in `nix/` shall be placed like seen further below.
    The tree is traversed by recursive decentral calls of the test evaluator
     function `evalTests` which at each internal tree node (read: directory)
     imports all files and directories, prefixed with `test_`, with some libraries
     and context.
    Unit tests must yield `true` on success or `false` on failure. All outcomes are
     accumulated per tree node with `nix/test.nix` outputting the final result.
    
                                     nix/
                                      │
                         ✼ test.nix ──┤
                              tests ──┤
                  • default.nix ──┤   │
                      test_yk8s ──┘   └── yk8s
              • default.nix ──┤           │
                   test_lib ──┤           ├── lib
          • default.nix ──┤   │           │   │
         test_types.nix ──┤   │       →   │   ├── types.nix
                          ┊   │           │   ┊
                              │           │
         test_terraform.nix ──┤       →   ├── terraform.nix
                              ┊           ┊
    
      ✼   entry point
      •   "runs `evalTests`" -> "imports tests and accumulates outcomes"
      →   "A imports B for testing"
    
    The CI pipeline recieved the new `unit-tests` stage + `test-nix-yk8s` job for
     running these unit tests.
    
    Co-Authored-By: Silvio Ankermann <silvio.ankermann@cloudandheat.com>

---
+++ nix/tests/default.nix
@@ -0,0 +1,153 @@
+{
+  lib,
+  importPath,
+}: let
+  # Directory that contains the tests
+  testsDir = ./.;
+
+  # Name prefix of test files and directories
+  # (only those get evaluated)
+  filePrefix = "test_";
+
+  /*
+  Run all the tests contained in a directory
+
+  Returns the outcome as a boolean where `true` means success and `false` failure.
+  Trace-outputs the files that are being evaluated and their individual outcomes.
+
+  Searches for test files and directories in the given path
+  and imports them with the following argument set:
+
+    {
+      lib,              # Nixpkgs library or equal
+      yk8s-test-lib,    # The yk8s test library
+      ctx = {           # Test context
+        evaluator,      # Test evaluator function (the importer itself)
+        importPath      # The importPath of the importer extended
+                        #  with the unprefixed name of the imported file or directory,
+                        #  e.g. importer.importPath = ./lorem;
+                        #       imported.name = ./tests/test_lorem/test_ipsum.nix;
+                        #       -> imported.importPath = ./lorem/ipsum.nix;
+      },
+      extra ? {}        # Optional attrset for passing extra arguments
+    }
+
+  A test file or directory needs to yield a boolean which determines the test's outcome.
+
+  Arguments (attrset):
+    - self: This function
+    - yk8s-test-lib: Attrset holding the yk8s test library
+    - path: The directory path to search for tests
+    - importPath: The path from where tests should import their target, e.g. ./nix/yk8s/lib/types.nix
+    - extra: Optional attrset with additional data or functions
+  */
+  evalTests = {
+    self,
+    yk8s-test-lib,
+    path,
+    importPath,
+    extra ? {},
+  }: let
+    inherit (builtins) baseNameOf foldl' isBool toString;
+    inherit (lib.trivial) boolToString;
+    inherit (lib.debug) traceSeq;
+    inherit (yk8s-test-lib) getTestFiles stripPrefix;
+    testFilePrefix = yk8s-test-lib.filePrefix;
+  in
+    # NOTE: Completely evaluate all tests first then accumulate results
+    #       so that the evaluation is not stopped by a negative result
+    #       and all of them are traced
+    # Accumulate test boolean outcomes
+    foldl' (acc: result: acc && result.outcome) true (
+      let
+        # Run each collected test file or directory
+        results = builtins.map (
+          file: let
+            # Import test file or directory
+            #  passing it lib, yk8s-test-lib, test context, a test evaluator and optional extra arguments
+            test_output = import file.path {
+              inherit lib yk8s-test-lib extra;
+              ctx = {
+                evaluator = self;
+                importPath =
+                  importPath
+                  + "/${stripPrefix testFilePrefix (baseNameOf file.path)}";
+              };
+            };
+            # Set boolean outcome
+            # (tests need to return booleans, if they don't they are deemed to have failed)
+            outcome =
+              if (isBool test_output)
+              then test_output
+              else false;
+          in
+            traceSeq "Evaluating ${toString file.path} ..."
+            {
+              outcome =
+                traceSeq "Outcome: ${toString file.path}: ${boolToString outcome}${
+                  if outcome != test_output
+                  then " (invalid output)"
+                  else ""
+                }"
+                outcome;
+            }
+          # collect test files and directories
+        ) (getTestFiles path);
+      in
+        # Return test result (which contains the outcome)
+        builtins.deepSeq results results # force complete evaluation
+    );
+
+  yk8s-test-lib = rec {
+    inherit
+      filePrefix
+      evalTests
+      ;
+
+    /*
+    Select files from a given file path whose type is one of the given ones
+
+    Arguments:
+      - types: A list of file types (types as output by `builtins.readDir`)
+      - path: A path
+    */
+    selectFilesByType = types: path:
+      lib.filterAttrs
+      (_: fileType: builtins.elem fileType types)
+      (builtins.readDir path);
+
+    /*
+    List all test files in a directory
+
+    Returns a list of attrsets where the 'path' attribute contains a file path
+    and the 'type' attribute is set to either "regular" or "directory".
+
+    Arguments:
+      - path: The directory's path
+    */
+    getTestFiles = path:
+      lib.attrsets.mapAttrsToList
+      (file: type: {
+        path = path + "/${file}";
+        type = type;
+      })
+      (
+        lib.filterAttrs
+        (name: _: lib.strings.hasPrefix filePrefix name)
+        (selectFilesByType ["regular" "directory"] path)
+      );
+
+    /*
+    Strip a prefix from a string
+    */
+    stripPrefix = prefix: str: let
+      inherit (builtins) head match;
+    in
+      head (match "^${prefix}(.+)$" str);
+  };
+in
+  evalTests {
+    inherit yk8s-test-lib importPath;
+    self = evalTests;
+    path = testsDir;
+  }
