b920c0aa502db5fb7bcf750f0a0ae1d0fa7a3f08
Steve Starke <steve.starke@cloudandheat.com>
2025-02-25 13:04:57 +0000

    Readd ability to configure mirrors for containerd
    This was a feature which we supported some time ago and which we want to
    readd. We're reusing some leftover/legacy code for that.

---
+++ nix/yk8s/containerd.nix
@@ -0,0 +1,67 @@
+{
+  config,
+  lib,
+  yk8s-lib,
+  ...
+}: let
+  cfg = config.yk8s.containerd;
+  inherit (lib) mkOption types;
+  inherit (yk8s-lib) mkTopSection mkGroupVarsFile;
+in {
+  options.yk8s.containerd = mkTopSection {
+    mirrors = mkOption {
+      description = ''
+        Registry mirrors which will be configured for containerd.
+        These can act as pull through cache to reduce external network traffic
+        and the amount of pulls from registries which have rate limits.
+        The upstream registry will automatically be used after all defined hosts have been tried.
+        To configure a default registry mirror, set "registry" to "_default".
+      '';
+      # TODO: type could be just listOf attrs in case we dont want to typecheck the whole set
+      type = types.listOf (types.submodule {
+        options = {
+          registry = mkOption {
+            type = types.nonEmptyStr;
+            description = ''
+              Name of the registry host for which the mirrors should be used.
+              Registry hosts are typically referred to by their internet domain names, aka. registry host names.
+              For example, docker.io, quay.io, gcr.io, and ghcr.io.
+              Set to "_default" if the mirrors should be used as default.
+            '';
+            example = "gcr.io";
+          };
+          mirrors = mkOption {
+            type = with types; listOf nonEmptyStr;
+            example = ["https://registry-1.example.com" "https://registry-2.example.com:5000"];
+            description = ''
+              A list of URLs which should be substituted for the registry.
+              Optionally specify a port.
+            '';
+          };
+        };
+      });
+      default = [];
+      example = [
+        {
+          registry = "docker.io";
+          mirrors = ["https://registry-cache-1.example.com" "https://registry-cache-2.example.com:5000"];
+        }
+        {
+          registry = "some.container.registry";
+          mirrors = ["https://registry-cache-3.example.com"];
+        }
+        {
+          registry = "_default";
+          mirrors = ["https://registry-cache-4.example.com"];
+        }
+      ];
+    };
+  };
+  config.yk8s._inventory_packages = [
+    (mkGroupVarsFile {
+      inherit cfg;
+      ansible_prefix = "containerd_";
+      inventory_path = "all/containerd.yaml";
+    })
+  ];
+}
--- nix/yk8s/miscellaneous.nix
+++ nix/yk8s/miscellaneous.nix
@@ -24,6 +24,8 @@ in {
     (mkRenamedOptionModuleWithNewSection "miscellaneous" "check_openstack_credentials" "openstack" "check_credentials")
     (mkRenamedOptionModuleWithNewSection "miscellaneous" "haproxy_frontend_nodeport_maxconn" "load-balancing" "haproxy_frontend_nodeport_maxconn")
     (mkRenamedOptionModuleWithNewSection "miscellaneous" "haproxy_frontend_k8s_api_maxconn" "load-balancing" "haproxy_frontend_k8s_api_maxconn")
+    (mkRemovedOptionModule "miscellaneous" "docker_registry_mirrors" "Use containerd.mirrors instead")
+    (mkRemovedOptionModule "miscellaneous" "container_mirrors" "Use containerd.mirrors instead")
   ];
   options.yk8s.miscellaneous = mkTopSection {
     _docs.preface = ''
@@ -86,16 +88,6 @@ in {
       type = types.int;
       default = 262144;
     };
-    docker_registry_mirrors = mkOption {
-      description = ''
-        Custom Docker Configuration
-        A list of registry mirrors can be configured as a pull through cache to reduce
-        external network traffic and the amount of docker pulls from dockerhub.
-      '';
-      type = with types; listOf nonEmptyStr;
-      default = [];
-      example = ["https://0.docker-mirror.example.org" "https://1.docker-mirror.example.org"];
-    };
     docker_insecure_registries = mkOption {
       description = ''
         Custom Docker Configuration
@@ -108,40 +100,6 @@ in {
     container_mirror_default_host = mkOption {
       type = types.nonEmptyStr;
       default = "install-node";
-    };
-    container_mirrors = mkOption {
-      # TODO: type could be just listOf attrs in case we dont want to typecheck the whole set
-      type = types.listOf (types.submodule {
-        options = {
-          name = mkOption {
-            type = types.nonEmptyStr;
-          };
-          upstream = mkOption {
-            type = types.nonEmptyStr;
-          };
-          port = mkOption {
-            type = with types; nullOr port;
-            default = null;
-          };
-          mirrors = mkOption {
-            type = with types; listOf nonEmptyStr;
-            default = [];
-          };
-        };
-      });
-      default = [];
-      example = [
-        {
-          name = "docker.io";
-          upstream = "https://registry-1.docker.io/";
-          port = 5000;
-        }
-        {
-          name = "gitlab.cloudandheat.com";
-          upstream = "https://registry.gitlab.cloudandheat.com/";
-          mirrors = ["https://install-node:8000"];
-        }
-      ];
     };
     custom_chrony_configuration = mkEnableOption ''
       custom Chrony configration
