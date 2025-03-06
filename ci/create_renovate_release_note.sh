074b86e1891e9c3bb62ee154908c4d82638cad96
Steve Starke <steve.starke@cloudandheat.com>
2025-03-06 11:53:42 +0000

    renovate: Create feature release notes for helm updates
    Default helm chart versions are managed by renovate-bot now (!1671).
    However, the renovate bot creates an empry release note of category chore.
    Updated helm chart versions may contain breaking changes a user needs to
    be aware of. We therefore should create a release note of category
    feature.

---
+++ ci/create_renovate_release_note.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# This script is used to create an empty release note
+# of the category "chore".
+# This script is triggered as "postUpgradeTasks"
+# by the renovate bot.
+
+DATE_STRING="$(date --iso-8601=seconds -u)"
+RELEASENOTE_PATH="docs/_releasenotes"
+
+DATASOURCE=$1
+DEP_NAME=$2
+SOURCE_URL=$3
+CURRENT_VERSION=$4
+NEW_VERSION=$5
+
+if [[ $DATASOURCE == 'helm' ]]; then
+  CATEGORY="feature"
+else
+  CATEGORY="chore"
+fi
+
+while [ -f "$RELEASENOTE_PATH/+.$CATEGORY.$DATE_STRING" ]; do
+  DATE_STRING="$(date --iso-8601=seconds -u)"
+done
+
+if [[ $CATEGORY == 'feature' && $DATASOURCE == 'helm' ]]; then
+  echo "Updated default version of helm chart $DEP_NAME of $SOURCE_URL from $CURRENT_VERSION to $NEW_VERSION" > "$RELEASENOTE_PATH/x.$CATEGORY.$DATE_STRING"
+else
+  touch "$RELEASENOTE_PATH/+.$CATEGORY.$DATE_STRING"
+fi
