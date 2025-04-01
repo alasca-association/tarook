63bb3e9775b3036e2297fc0370f5294936e31a35
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2025-04-01 16:51:01 +0200

    nix lib: add mkHelmValuesOption

--- nix/yk8s/lib/options.nix
+++ nix/yk8s/lib/options.nix
@@ -1,7 +1,4 @@
-{lib}: let
-  types = import ./types.nix {inherit lib;};
-  transform = import ./transform.nix {inherit lib;};
-in rec {
+{lib}: rec {
   /*
   Like nixpkgs.lib.options.mkEnableOption but with true as the default
   */
@@ -54,4 +51,70 @@ in rec {
     // opts);
   mkTopSection = mkSection {sectionType = "top";};
   mkSubSection = mkSection {sectionType = "sub";};
+
+  /*
+  Returns an option representing the values of a Helm chart.
+
+  A name must be given, all other parameters are optional.
+
+  defaultValues can be given but note that they will be lost completely if the user
+  sets any value themselves (even if they set different values than are set in defaultValues).
+  For all values that should persist additionally to any user specified values, add them as
+  chartOptions with their own default value.
+
+  !!IMPORTANT!!
+  Note that the option must be added to the unflat list of mkGroupVarsFile
+
+  Example:
+
+  {
+    options.yk8s.my-module.helm_values = mkHelmValuesOption {
+      name = "my-application";
+      defaultValues = {
+        A = "this setting will be lost as soon as anything else is set by the user or by a module, even if A is not set there"
+      };
+      chartOptions = {
+        B = lib.mkOption {
+          # ...
+          default = "this setting will persist if other options are set but will be overwritten if B is set somewhere else";
+      };
+    };
+    config.yk8s.my-module.helm_values = {
+      C = "this setting will persist if other options are set and will create a conflict if C is set somewhere else. In order to override it, lib.mkForce must be used";
+    };
+    config.yk8s._inventory_packages = [
+      (mkGroupVarsFile {
+        inherit (config.yk8s) my-module;
+        ansible_prefix = "my_prefix_";
+        inventory_path = "all/my_module.yaml";
+        unflat = ["helm_values"];
+        })
+    ];
+  }
+  */
+  mkHelmValuesOption = {
+    name,
+    valuesDocUrl ? null,
+    extraDescription ? "",
+    chartOptions ? {},
+    defaultValues ? {},
+  }:
+    lib.mkOption {
+      description =
+        ''
+          Helm values for the ${name} helm chart.
+
+          Some values are set by default through Tarook, but arbitrary values can be set.
+        ''
+        + (lib.optionalString (valuesDocUrl != null) ''
+          For a full list of possible values, see
+          ${valuesDocUrl}
+        '')
+        + extraDescription;
+      default = defaultValues;
+      type = lib.types.submodule {
+        freeformType = yk8s-lib.types.jsonValue;
+        options = chartOptions;
+      };
+    };
 }
