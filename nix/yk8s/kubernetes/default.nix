573c12b88dbcaf442eb19615727986c578b9a299
Steve Starke <steve.starke@cloudandheat.com>
2025-07-23 09:12:26 +0200

    Refine audit-logs for kube-apiserver
    The previous attempt has been refined such that it is now possible to
    finely configure the audit-policy manifest. The "custom_policy" option
    has been removed in favor of a generic option which is built properly
    with submodules and has defaults. This also ensures the default values
    are documented in the reference documentation.
    
    Co-authored-by: Silvio Ankermann <silvio.ankermann@cloudandheat.com>

--- nix/yk8s/kubernetes/default.nix
+++ nix/yk8s/kubernetes/default.nix
@@ -8,10 +8,11 @@
   modules-lib = import ../lib/modules.nix {inherit lib;};
   inherit (modules-lib) mkRemovedOptionModule mkRenamedOptionModuleWithNewSection;
   inherit (lib) mkOption mkEnableOption types;
-  inherit (yk8s-lib) mkTopSection mkGroupVarsFile;
+  inherit (yk8s-lib) mkTopSection mkGroupVarsFile mkInternalOption mkYaml;
   inherit
     (yk8s-lib.transform)
     warnIfZero
+    filterNull
     ;
   inherit
     (yk8s-lib.types)
@@ -27,6 +28,7 @@ in {
     (mkRemovedOptionModule "kubernetes" "continuous_join_key" "")
     (mkRenamedOptionModuleWithNewSection "kubernetes" "monitoring.alertmanager_config_secret" "k8s-service-layer.prometheus" "alertmanager_config_secret")
     (mkRemovedOptionModule "kubernetes" "global_monitoring" "This section has been moved to a custom role")
+    (mkRemovedOptionModule "kubernetes" "apiserver.audit_logs.custom_policy" "Use config.yk8s.kubernetes.apiserver.audit_logs.policy instead")
     (mkRenamedOptionModuleWithNewSection "kubernetes" "storage.rook_enabled" "k8s-service-layer.rook" "enabled")
     (mkRenamedOptionModuleWithNewSection "kubernetes" "storage.cinder_enable_topology" "openstack" "cinder_enable_topology")
   ];
@@ -82,55 +84,56 @@ in {
         default = null;
         example = "1Gi";
       };
-      audit_logs.enabled = mkEnableOption "audit logs for the apiserver.";
-      audit_logs.max_size = mkOption {
-        description = ''
-          Maximum size of apiserver audit log files in megabytes before it gets rotated
-        '';
-        type = types.int;
-        default = 50;
-      };
-      audit_logs.custom_policy = mkOption {
-        description = ''
-          Provide a custom audit policy for the kube-apiserver. Overrides the default audit policy.
+      audit_logs = {
+        enabled = mkEnableOption ''
+          audit logs for the apiserver.
 
-          You may use ``audit_logs_policy = yk8s-lib.importYAML ./path/to/policy.yaml;`` to import
-          an existing policy manifest. Note that the YAML file has to be added to the git repository
-          to be evaluated by Nix.
+          .. note::
 
-          Alternatively, if you prefer to specify the policy in Nix directly, you may use
-          https://github.com/cloudandheat/json2nix to convert existing policies to Nix.
-        '';
-
-        example = {
-          omitStages = [
-            "RequestReceived"
-          ];
-          rules = [
-            {
-              level = "None";
-              nonResourceURLs = [
-                "/healthz*"
-                "/readyz"
-                "/"
-                "/livez"
-                "/version"
-                "/swagger*"
-              ];
-            }
-          ];
+            Modifications to this setting and its related only apply during Kubernetes upgrades'';
+        max_size = mkOption {
+          description = ''
+            Maximum size of apiserver audit log files in megabytes before it gets rotated
+          '';
+          type = types.ints.unsigned;
+          default = 50;
         };
-        default = null;
-        type = with lib.types; nullOr attrs;
-        apply = v:
-          if v == null
-          then null
-          else
-            v
-            // {
-              apiVersion = v.apiVersion or "audit.k8s.io/v1";
-              kind = v.kind or "Policy";
+        policy = let
+          defaultsFile = ./audit-policy-defaults.nix;
+        in
+          mkOption {
+            description = ''
+              The audit policy for the kube-apiserver.
+              Checkout the
+              `Kubernetes Auditing Documentation <https://kubernetes.io/docs/tasks/debug/debug-cluster/audit/#audit-policy>`_
+              for further information on how to configure.
+
+              You may use ``audit_logs.policy = yk8s-lib.importYAML ./path/to/policy.yaml;`` to import
+              an existing policy manifest. Note that the YAML file has to be added to the git repository
+              in order to be evaluated by Nix.
+
+              Alternatively, if you prefer to specify the policy in Nix directly, you may use
+              https://github.com/cloudandheat/json2nix to convert existing policies to Nix.
+
+              Note that this option is not type checked by Nix, so make sure that it it's a valid audit policy.
+            '';
+
+            example = lib.options.literalExample ''
+              yk8s-lib.importYAML ./path/to/policy.yaml # Note that the file has to be added to the git repository to be evaluated by Nix
+            '';
+            type = lib.types.submodule {
+              freeformType = lib.types.attrs;
             };
+            default = import defaultsFile;
+
+            # to include the comments in the docs
+            defaultText = lib.options.literalExpression (builtins.readFile defaultsFile);
+          };
+        policy_file = mkInternalOption {
+          readOnly = true;
+          type = types.pathInStore;
+          default = toString (mkYaml "audit-policy.yaml" (filterNull cfg.apiserver.audit_logs.policy));
+        };
       };
     };
     controller_manager = {
@@ -168,7 +171,10 @@ in {
       inherit cfg;
       ansible_prefix = "k8s_";
       inventory_path = "all/kubernetes.yaml";
-      unflat = ["apiserver.audit_logs.custom_policy"];
+      transformations = [
+        # `apiserver.audit_logs.policy` is removed because it is proxied by `apiserver.audit_logs.policy_file`
+        (cfg: yk8s-lib.transform.removeAttrByPath cfg ["apiserver" "audit_logs" "policy"])
+      ];
     })
   ];
 }
