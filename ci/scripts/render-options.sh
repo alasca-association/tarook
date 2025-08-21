0339544f81f198e2290d9c7f00b66ebdea5e4443
Steve Starke <steve.starke@cloudandheat.com>
2025-08-21 09:51:43 +0200

    Restructure CI

--- ci/scripts/render-options.sh
+++ ci/scripts/render-options.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# shellcheck source=ci/housekeeping/lib.sh
+# shellcheck source=ci/scripts/lib.sh
 . "$(dirname "$0")/lib.sh"
 
 run nix build --no-link .#referenceOptionsRST
 out="$(nix build --print-out-paths --no-link .#referenceOptionsRST)"
 DIFF_PATH=./docs/user/reference/options
 run rsync -rL --delete --chmod 664 "$out/" "${DIFF_PATH}"
-COMMIT_MSG="Update rendered docs"
+export COMMIT_MSG="Update rendered docs"
 push_if_changed
