5ca2d977c20582466d8b199c049f643bf188e279
Steve Starke <steve.starke@cloudandheat.com>
2026-01-30 12:09:49 +0100

    Revert "chore(deps): update helm release flux2 to v2.17.0"
    This reverts commit 8f29398f996f203e53a28a7076b891927fe6e22b.
    
    Upgrading to this version requires additional manual steps (upgrading
    the CRDs or running "flux migrate") which have not been automated, yet.
    To not postpone the next release, we're reverting to the last version to
    work.

--- nix/yk8s/k8s-supplements/fluxcd.nix
+++ nix/yk8s/k8s-supplements/fluxcd.nix
@@ -40,7 +40,7 @@ in {
       '';
       type = types.yk8s.oci.imageTag;
       # renovate: datasource=helm depName=flux2 registryUrl=https://fluxcd-community.github.io/helm-charts
-      default = "2.17.0";
+      default = "2.15.0";
     };
     namespace = mkOption {
       description = ''
