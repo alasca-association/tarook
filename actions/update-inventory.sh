a84fb92afad9fc4dd8aac067326423592436d085
Silvio Ankermann <silvio.ankermann@cloudandheat.com>
2026-04-24 14:24:11 +0200

    Fix TAROOK_NIX_FLAGS to allow multiple flags
    Otherwise passing two or more flags separated by a space results in an
    error message like
    
    error: unrecognised flag '--debug --show-trace'

--- actions/update-inventory.sh
+++ actions/update-inventory.sh
@@ -66,11 +66,10 @@ if [ -z "${valid_target:-}" ]; then
 fi
 
 if [[ -e "state" ]]; then git add state; fi
-if [ -z "${TAROOK_NIX_FLAGS:-}" ]; then
-    out=$(nix build --override-input yk8s "$code_repository" --print-out-paths --no-link ".#yk8s-outputs-$target")
-else
-    out=$(nix build --override-input yk8s "$code_repository" --print-out-paths --no-link "${TAROOK_NIX_FLAGS}" ".#yk8s-outputs-$target")
-fi
+set -f # disable glob expansion because we need to pass TAROOK_NIX_FLAGS unquoted
+ # shellcheck disable=SC2086
+out=$(nix build --override-input yk8s "$code_repository" --print-out-paths --no-link ${TAROOK_NIX_FLAGS} ".#yk8s-outputs-$target")
+set +f
 # shellcheck disable=SC1091
 . "$out/.path-info"
 # shellcheck disable=SC2154
