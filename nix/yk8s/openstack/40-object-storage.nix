267a1b2fa983a4857ea67cb50ff883cc02450e8a
Steve Starke <steve.starke@cloudandheat.com>
2026-07-02 15:27:01 +0200

    terranix: Fix managing Thanos container
    The "thanos_data" resource must be only created if
    "manage_thanos_bucket" is enabled. The previous code created the
    resource, but with no attributes which then caused Terraform to fail.
    
    Regression-of: !2545

--- nix/yk8s/openstack/40-object-storage.nix
+++ nix/yk8s/openstack/40-object-storage.nix
@@ -6,10 +6,14 @@
   cfg = config.yk8s.openstack;
 in {
   yk8s.terraform.modules = lib.optional cfg.enabled {
-    resource."openstack_objectstorage_container_v1"."thanos_data" = lib.optionalAttrs cfg.monitoring_manage_thanos_bucket {
-      _import_from = "openstack_objectstorage_container_v1.thanos_data[0]";
-      name = "${config.yk8s.infra.cluster_name}-monitoring-thanos-data";
-      force_destroy = cfg.thanos_delete_container;
-    };
+    resource."openstack_objectstorage_container_v1" =
+      lib.optionalAttrs cfg.monitoring_manage_thanos_bucket
+      {
+        "thanos_data" = {
+          _import_from = "openstack_objectstorage_container_v1.thanos_data[0]";
+          name = "${config.yk8s.infra.cluster_name}-monitoring-thanos-data";
+          force_destroy = cfg.thanos_delete_container;
+        };
+      };
   };
 }
