cf19fc35c50c8b8776991905e460cf04de710b4f
Steve Starke <steve.starke@cloudandheat.com>
2026-07-02 14:40:27 +0200

    terranix: Fix state migration for Thanos container
    Must be an attrs, not an array. Otherwise the "_import_from" key is not
    filtered in [1] and the release migration fails if
    "monitoring_manage_thanos_bucket" is enabled.
    
    [1] https://gitlab.com/alasca.cloud/tarook/tarook/-/blob/55cb2e9969083bd814fde2195d5a0116b5ee06e7/nix/yk8s/terraform.nix#L249

--- nix/yk8s/openstack/40-object-storage.nix
+++ nix/yk8s/openstack/40-object-storage.nix
@@ -6,7 +6,7 @@
   cfg = config.yk8s.openstack;
 in {
   yk8s.terraform.modules = lib.optional cfg.enabled {
-    resource."openstack_objectstorage_container_v1"."thanos_data" = lib.optional cfg.monitoring_manage_thanos_bucket {
+    resource."openstack_objectstorage_container_v1"."thanos_data" = lib.optionalAttrs cfg.monitoring_manage_thanos_bucket {
       _import_from = "openstack_objectstorage_container_v1.thanos_data[0]";
       name = "${config.yk8s.infra.cluster_name}-monitoring-thanos-data";
       force_destroy = cfg.thanos_delete_container;
